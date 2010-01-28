a38830cd3e72f6bb1783a7b6bef2563d4c056cd6
hillerj <hillerj@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-01-28 21:32:30 +0000

    fixed BaseaSlotComponents class in BaseaBlogPostSlotComponents.class.php and BaseaBlogCalendarSlotComponents.class.php
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@93 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aCalendar/templates/_upcomingEvents.php
+++ modules/aCalendar/templates/_upcomingEvents.php
@@ -2,10 +2,10 @@
   <?php foreach ($a_blog_events as $a_blog_event): ?>
     
     <div class="a-calendar-event">
+	    <h3 class="a-blog-post-title"><?php echo link_to($a_blog_event->getTitle(), 'a_calendar_post', $a_blog_event) ?></h3>
       <div class="a-calendar-meta">
         <ul>
-          <li class="a-calendar-date"><?php echo date('l', strtotime($a_blog_event->getStartDate())) ?></li>
-          <li><?php echo date('F jS Y', strtotime($a_blog_event->getStartDate())) ?></li>
+	        <li class="a-calendar-date"><?php echo date('l, F jS Y', strtotime($a_blog_event->getStartDate())) ?></li>
           <?php if($a_blog_event->getStartTime()): ?>
           <li><?php echo date('g:iA', strtotime($a_blog_event->getStartTime())) ?> 
           <?php if($a_blog_event->getEndTime()): ?>- <?php echo date('g:iA', strtotime($a_blog_event->getEndTime())) ?></li>
@@ -15,7 +15,6 @@
       </div>
     
       <div class="a-calendar-event-body">
-        <h3 class="a-blog-post-title"><?php echo link_to($a_blog_event->getTitle(), 'a_calendar_post', $a_blog_event) ?></h3>
         <?php if (str_word_count(strip_tags($a_blog_event->getBody())) > 30): ?>
           <?php echo ($a_blog_event->getExcerpt()) ? $a_blog_event->getExcerpt() : $a_blog_event->getPreview(30) ?>
           <span class='a-blog-read-more'><?php echo link_to('Read More', 'a_calendar_post', $a_blog_event, array('class' => 'a-blog-more')) ?></span>
@@ -27,4 +26,3 @@
     
   <?php endforeach ?>
 </div>
-
