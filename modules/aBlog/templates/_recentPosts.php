b914ea53d8542e212a2a75d15f3cf777c0a0b43b
hillerj <hillerj@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-02-08 22:06:28 +0000

    changed recent posts formatting to match other templates
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.3@212 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_recentPosts.php
+++ modules/aBlog/templates/_recentPosts.php
@@ -1,9 +1,13 @@
 <ul class="a-blog-recentposts">
 	<?php foreach ($a_blog_posts as $a_blog_post): ?>
 		<li>
+			<div class="a-blog-post">
 			<h3 class="a-blog-post-title"><?php echo link_to($a_blog_post->getTitle(), 'a_blog_post', $a_blog_post) ?></h3>
-			<p><?php echo ($a_blog_post->getExcerpt()) ? $a_blog_post->getExcerpt() : $a_blog_post->getPreview() ?></p>
-			<p><?php echo link_to('Read More', 'a_blog_post_show', $a_blog_post, array('class' => 'a-blog-more')) ?></p>
+			<div class="a-blog-post-excerpt-container">
+				<?php echo ($a_blog_post->getExcerpt()) ? $a_blog_post->getExcerpt() : $a_blog_post->getPreview(30) ?>
+				<div class="a-blog-read-more"><?php echo link_to('Read More', 'a_blog_post', $a_blog_post, array('class' => 'a-blog-more')) ?></div>
+			</div>
+			</div>
 		</li>
 	<?php endforeach ?>
 </ul>
