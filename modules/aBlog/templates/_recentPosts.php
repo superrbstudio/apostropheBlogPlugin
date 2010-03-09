6436dcb2d7dc7a301d1ef00b2a802e51efa1cecb
hillerj <hillerj@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-03-09 15:17:30 +0000

    added images to recent posts component
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.3@535 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_recentPosts.php
+++ modules/aBlog/templates/_recentPosts.php
@@ -1,8 +1,24 @@
 <ul class="a-blog-recentposts">
 	<?php foreach ($a_blog_posts as $a_blog_post): ?>
 		<li>
-			<div class="a-blog-post">
+			<div class="a-blog-post <?php echo count($a_blog_post->getAttachedMedia()) > 0? 'contains-media' : ''?> ">
 			<h3 class="a-blog-post-title"><?php echo link_to($a_blog_post->getTitle(), 'a_blog_post', $a_blog_post) ?></h3>
+			<?php if ($a_blog_post->getAttachedMedia()): ?>
+				<?php if (in_array('aSlideshowSlot', sfConfig::get('sf_enabled_modules'))): ?>
+					<div class="a-blog-post-media">
+						<?php include_component('aSlideshowSlot', 'slideshow', array(
+							'items' => $a_blog_post->getAttachedMedia(),
+							'id' => $a_blog_post->getId(),
+							'options' => array(
+								'width' => isset($options['aSlideshow']['width'])? $options['aSlideshow']['width'] : 290, 
+								'height' => isset($options['aSlideshow']['height'])? $options['aSlideshow']['height'] : 210, 
+								'resizeType' => isset($options['aSlideshow']['resizeType'])? $options['aSlideshow']['resizeType'] : 'c',
+								'flexheight' => isset($options['aSlideshow']['flexheight'])? $options['aSlideshow']['flexheight'] : false, 
+								'arrows' => isset($options['aSlideshow']['arrows'])? $options['aSlideshow']['arrows'] : false )
+						)) ?>
+					</div>
+				<?php endif ?>
+			<?php endif ?>
 			<div class="a-blog-post-excerpt-container">
 				<?php echo ($a_blog_post->getExcerpt()) ? $a_blog_post->getExcerpt() : $a_blog_post->getPreview(30) ?>
 				<div class="a-blog-read-more"><?php echo link_to('Read More', 'a_blog_post', $a_blog_post, array('class' => 'a-blog-more')) ?></div>
