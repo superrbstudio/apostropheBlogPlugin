b4a13f24acbab71613b6b58b9a669e2b80c98184
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-03-26 17:40:52 +0000

    created an assets override for blog admin to ensure that blog.css is included
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@737 c89fab08-2f9b-42bf-8176-bd3ad6bee547

---
+++ modules/aBlogAdmin/templates/__assets.php
@@ -0,0 +1,12 @@
+<?php slot('body_class') ?>a-admin <?php echo $sf_params->get('module'); ?> <?php echo $sf_params->get('action');?> <?php end_slot() ?>
+
+	<?php use_stylesheet('/apostrophePlugin/css/a.css', 'first') ?>
+	<?php use_stylesheet('/apostropheBlogPlugin/css/blog.css') ?>
+
+	<?php use_javascript('/apostrophePlugin/js/aControls.js') ?>
+	<?php use_javascript('/apostrophePlugin/js/aUI.js') ?>
+
+	<?php use_stylesheet('/sfJqueryReloadedPlugin/css/ui-lightness/jquery-ui-1.7.2.custom.css', 'first') # JQ Date Picker Styles (This doesn't have to be the ui.all.css, we could make a custom css later ) ?>
+	<?php use_javascript('/sfJqueryReloadedPlugin/js/plugins/jquery-ui-1.7.2.custom.min.js', 'last') # JQ Date Picker JS (This can/should be consolidated with sfJqueryReloadedPlugin/js/jquery-ui-sortable...) ?>
+
+<?php aTools::setAllowSlotEditing(false); ?>
