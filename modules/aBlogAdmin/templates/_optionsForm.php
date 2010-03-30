affe177b458bcad356b29b609cb272258027ce3a
dordille <dordille@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-03-30 16:24:31 +0000

    Added tag typeahead to blog editor #211.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@783 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlogAdmin/templates/_optionsForm.php
+++ modules/aBlogAdmin/templates/_optionsForm.php
@@ -43,6 +43,13 @@
 	<h2>Tags</h2>
 	<?php echo $form['tags']->render() ?>
 	<?php echo $form['tags']->renderError() ?>
+  <script src='/sfDoctrineActAsTaggablePlugin/js/pkTagahead.js'></script>
+  <script type="text/javascript" charset="utf-8">
+    $(function() {
+      pkTagahead(<?php echo json_encode(url_for("taggableComplete/complete")) ?>);
+    });
+</script>
+
   <?php include_component('aBlogAdmin','tagList', array('a_blog_post' => $form->getObject())) ?>
 </div>
 
