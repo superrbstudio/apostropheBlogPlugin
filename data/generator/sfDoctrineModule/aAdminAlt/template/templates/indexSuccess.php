2f8678fbdeb4bbdf5a89c64fa66c25143ef743a8
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-02 20:36:25 +0000

    the button menu things for the index are finally done
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@839 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- data/generator/sfDoctrineModule/aAdminAlt/template/templates/indexSuccess.php
+++ data/generator/sfDoctrineModule/aAdminAlt/template/templates/indexSuccess.php
@@ -5,10 +5,13 @@
 $(document).ready(function(){
 	var sortLabel = $("a.a-sort-label").parent().parent();
 	sortLabel.click(function(event) {
-		event.preventDefault();		
+		event.preventDefault();				
+		var thisSortLabel = $(this).find('a.a-sort-label');
+		thisSortLabel.focus();
     $(this).addClass('show-filters').find(".filternav").show();
     $(this).parent().hover(function() {
     }, function() {
+			thisSortLabel.blur();
 			$(this).find('ul').removeClass('show-filters');
       $(this).find(".filternav").fadeOut();
     });
