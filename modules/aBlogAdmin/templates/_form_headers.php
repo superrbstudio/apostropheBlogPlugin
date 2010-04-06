c396a0f3ff34a279d3686294fff402b6351023c1
dordille <dordille@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-06 19:58:09 +0000

    Removed unused template.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@864 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlogAdmin/templates/_form_headers.php
+++
@@ -1,7 +0,0 @@
-<p><?php echo link_to('Edit Posts', '@a_blog_admin', array('class' => 'a-btn icon a-edit', )) ?></p>
-<br/><br/>
-<p><?php echo link_to('New  Posts', '@a_blog_admin_new', array('class' => 'a-btn icon a-edit', )) ?></p>
-<br/><br/>
-<?php echo link_to('Edit Categories', '@a_blog_category_admin', array('class' => 'a-btn icon a-edit', )) ?>
-<br/><br/>
-<?php echo link_to('Edit Comments', '@a_comment_admin', array('class' => 'a-btn icon a-edit', )) ?>
