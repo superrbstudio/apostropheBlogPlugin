cb638bf2bff99a8fb226431a83dbff893e58a424
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-08 16:14:59 +0000

    I have a feeling these files are going to be here to stay, so I gave them snazzier file names. Remember to change them in your view.yml if you're using them
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@887 c89fab08-2f9b-42bf-8176-bd3ad6bee547

---
+++ web/js/aBlog.js
@@ -0,0 +1,42 @@
+// Publish / Unpublish Button
+function checkAndSetPublish(slug_url)
+{
+	var postStatus = $('#a_blog_post_status');
+
+	if (postStatus.val() == 'published') {
+		$('#a-admin-form a.a-publish-post').addClass('published');
+	};
+
+	$('#a-admin-form a.a-publish-post').unbind('click').click(function(){
+
+		$(this).toggleClass('published');
+		$(this).blur();
+
+		if (postStatus.val() == 'draft') {
+			postStatus.val('published');
+		}
+		else
+		{
+			postStatus.val('draft');
+		};
+		updateBlogForm(slug_url);
+	});			
+}
+
+
+// Ajax Update Blog Form
+function updateBlogForm(slug_url)
+{
+	$.ajax({
+	  type:'POST',
+	  dataType:'html',
+	  data:jQuery('#a-admin-form').serialize(),
+	  success:function(data, textStatus)
+		{
+			$('#a-admin-blog-post-form').html(data);
+			aUI('#a-admin-form');
+			Cufon.refresh(); // This is specific to this project, how do we want to handle special calls? an function override at the project level?
+	 	},
+	 	url: slug_url
+	});
+}
