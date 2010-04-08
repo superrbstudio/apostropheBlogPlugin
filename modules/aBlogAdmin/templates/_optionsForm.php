ccfaaf05510d8dc1a307cf4dce4834d25a98187e
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-08 21:41:26 +0000

    changed the markup for the Publish button some
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@898 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlogAdmin/templates/_optionsForm.php
+++ modules/aBlogAdmin/templates/_optionsForm.php
@@ -5,7 +5,10 @@
 
 <div class="published section">
 
-	<a href="#" class="a-btn big a-publish-post <?php echo ($a_blog_post['status'] == 'published')? 'published':'' ?>" onclick="return false;"><em class="publish"><?php echo __('Publish', array(), 'messages') ?></em><em class="unpublish"><?php echo __('Unpublish', array(), 'messages') ?></em></a>
+	<a href="#" class="a-btn big a-publish-post <?php echo ($a_blog_post['status'] == 'published')? 'published':'' ?>" onclick="return false;">
+		<div class="publish"><?php echo __('Publish', array(), 'messages') ?></div>
+		<div class="unpublish"><?php echo __('Unpublish', array(), 'messages') ?></div>
+	</a>
 
 	<div class="post-status option">
 		<?php echo $form['status']->renderRow() ?>
