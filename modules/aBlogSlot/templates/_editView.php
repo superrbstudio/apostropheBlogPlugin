f6f07ddc7b22e3614117e503e1c9f2d506dcd5b5
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-22 20:20:10 +0000

    reworked aBlog slot edit view form
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1016 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlogSlot/templates/_editView.php
+++ modules/aBlogSlot/templates/_editView.php
@@ -1,17 +1,31 @@
 <?php // Just echo the form. You might want to render the form fields differently ?>
-<?php echo $form ?>
+
+<?php echo $form->renderHiddenFields() ?>
+
+<div class="a-form-row count">
+<?php echo $form['count']->renderLabel() ?>
+<div class="a-form-field"><?php echo $form['count']->render() ?></div>
+<div class="a-form-error"><?php echo $form['count']->renderError() ?></div>
+</div>
+
+<div class="a-form-row categories">
+<?php echo $form['categories_list']->renderLabel() ?>
+<div class="a-form-field"><?php echo $form['categories_list']->render() ?></div>
+<div class="a-form-error"><?php echo $form['categories_list']->renderError() ?></div>
+</div>
+
+<div class="a-form-row tags">
+<?php echo $form['tags_list']->renderLabel() ?>
+<div class="a-form-field"><?php echo $form['tags_list']->render() ?></div>
+<div class="a-form-error"><?php echo $form['tags_list']->renderError() ?></div>
+</div>
 
 <script src='/sfDoctrineActAsTaggablePlugin/js/pkTagahead.js'></script>
 <script type="text/javascript" charset="utf-8">
-  $(function() {
+$(document).ready(function() {
     pkTagahead(<?php echo json_encode(url_for("taggableComplete/complete")) ?>);
+    aMultipleSelect('#a-<?php echo $form->getName() ?>', { 'choose-one': 'Add Categories' });
+		$('#a-<?php echo $form->getName() ?>').addClass('a-options dropshadow');			
   });
 </script>
-
-<script type="text/javascript">
-  $(function() {
-    aMultipleSelect('#a-<?php echo $form->getName() ?>', { 'choose-one': 'Add Categories' })
-  });
-</script>
-
 
