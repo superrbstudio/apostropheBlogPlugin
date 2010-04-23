ee11d723df5a0feee65b7a8e84726029ab7d64e8
dordille <dordille@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-23 18:18:54 +0000

    TH headers now have sort functionality.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1027 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- data/generator/sfDoctrineModule/aBlogAdmin/template/templates/_list_th_tabular.php
+++ data/generator/sfDoctrineModule/aBlogAdmin/template/templates/_list_th_tabular.php
@@ -4,7 +4,7 @@
 <?php foreach ($this->configuration->getValue('list.display') as $name => $field): ?>
 	[?php slot('a-admin.current-header') ?]
 	<th class="a-admin-<?php echo strtolower($field->getType()) ?> a-column-<?php echo $name ?>">
-	<?php if(isset($filterFieldConfig[$name])): ?>
+  <?php if(isset($filterFieldConfig[$name])): ?>
     <?php //This field needs dropdown filters to be applied ?>
     <ul>
       <li><a href="#" class="a-btn a-sort-label">[?php echo __('<?php echo $field->getConfig('label') ?>', array(), '<?php echo $this->getI18nCatalogue() ?>') ?]</a>
@@ -20,19 +20,25 @@
         </div>
       </li>
     </ul>
-  
+  <?php else: ?>
+    <span>[?php echo __('<?php echo $field->getConfig('label') ?>', array(), '<?php echo $this->getI18nCatalogue() ?>') ?]</span>
+  <?php endif; ?>
 
-<?php elseif ($field->isReal()): ?>
-[?php if ('<?php echo $name ?>' == $sort[0]): ?]
-  [?php echo jq_link_to_function(__('<?php echo $field->getConfig('label') ?>', array(), '<?php echo $this->getI18nCatalogue() ?>'), 'getFilters("<?php echo $field->getName() ?>")') ?]
-  [?php echo image_tag(((sfConfig::get('app_aAdmin_web_dir'))?sfConfig::get('app_aAdmin_web_dir'):'/apostrophePlugin').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'a-admin'), 'title' => __($sort[1], array(), 'a-admin'))) ?]
- [?php else: ?]
-  [?php echo jq_link_to_function(__('<?php echo $field->getConfig('label') ?>', array(), '<?php echo $this->getI18nCatalogue() ?>'), 'getFilters("<?php echo $field->getName() ?>")') ?]
- [?php endif; ?]
-<?php else: ?>
-[?php echo jq_link_to_function(__('<?php echo $field->getConfig('label') ?>', array(), '<?php echo $this->getI18nCatalogue() ?>'), 'getFilters("<?php echo $field->getName() ?>")') ?]
-<?php endif; ?>
-  <div id="a-admin-list-th-<?php echo $name ?>-filter"></div>
+  <?php if ($field->isReal()): ?>
+  
+  [?php if ('<?php echo $name ?>' == $sort[0]): ?]
+    [?php echo link_to(
+      image_tag(((sfConfig::get('app_aAdmin_web_dir'))?sfConfig::get('app_aAdmin_web_dir'):'/apostrophePlugin').'/images/'.$sort[1].'-on.png', array('alt' => __($sort[1], array(), 'a-admin'), 'title' => __($sort[1], array(), 'a-admin'))),
+      '<?php echo $this->getModuleName() ?>/index?sort=<?php echo $name ?>&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc')
+      ) ?]
+    [?php else: ?]
+    [?php echo link_to(
+      image_tag(((sfConfig::get('app_aAdmin_web_dir'))?sfConfig::get('app_aAdmin_web_dir'):'/apostrophePlugin').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'a-admin'), 'title' => __($sort[1], array(), 'a-admin'))),
+      '<?php echo $this->getModuleName() ?>/index?sort=<?php echo $name ?>&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc')
+      ) ?]
+  [?php endif; ?]
+
+  <?php endif ?>
 	</th>
 	[?php end_slot(); ?]
 
