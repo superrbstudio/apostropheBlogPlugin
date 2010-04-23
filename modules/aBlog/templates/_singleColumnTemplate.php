81dfe9cf51fdcd46336efe5e6bd5346c3dfba63a
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-23 20:02:47 +0000

    changed the default slots included with the apostropheBlogPlugin templates to be Richtext, Slideshow, Video, and PDF
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1036 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_singleColumnTemplate.php
+++ modules/aBlog/templates/_singleColumnTemplate.php
@@ -1,11 +1,9 @@
 <?php a_area('blog-body', array(
   'editable' => false, 'toolbar' => 'basic', 'slug' => 'aBlog-'.$a_blog_post['id'],
-  'allowed_types' => array('aRichText', 'aImage', 'aButton', 'aSlideshow', 'aVideo'),
+  'allowed_types' => array('aRichText', 'aSlideshow', 'aVideo', 'aPDF'),
   'type_options' => array(
     'aRichText' => array('tool' => 'Main'),   
-    'aImage' => array('width' => 680, 'flexHeight' => true, 'resizeType' => 's', ),
-    'aButton' => array('width' => 680, 'flexHeight' => true, 'resizeType' => 's', 'constraints' => array('minimum-width' => 680)),
     'aSlideshow' => array("width" => 680, "flexHeight" => true, 'resizeType' => 's', 'constraints' => array('minimum-width' => 680)),
 		'aVideo' => array('width' => 680), 
-  ))
-) ?>
+		'aPDF' => array('width' => 680, 'flexHeight' => true, 'resizeType' => 's'),				
+))) ?>
--- modules/aBlog/templates/_twoColumnTemplate.php
+++ modules/aBlog/templates/_twoColumnTemplate.php
@@ -1,23 +1,19 @@
 <?php a_area('blog-body', array(
   'editable' => false, 'toolbar' => 'basic', 'slug' => 'aBlog-'.$a_blog_post['id'],
-  'allowed_types' => array('aRichText', 'aImage', 'aButton', 'aSlideshow', 'aVideo'),
+  'allowed_types' => array('aRichText', 'aSlideshow', 'aVideo', 'aPDF'),
   'type_options' => array(
     'aRichText' => array('tool' => 'Main'),   
-    'aImage' => array('width' => 480, 'flexHeight' => true, 'resizeType' => 's', 'constraints' => array('minimum-width' => 480)),
-    'aButton' => array('width' => 480, 'flexHeight' => true, 'resizeType' => 's', 'constraints' => array('minimum-width' => 480)),
     'aSlideshow' => array("width" => 480, "flexHeight" => true, 'resizeType' => 's', 'constraints' => array('minimum-width' => 480)),
 		'aVideo' => array('width' => 480), 
-  ))
-) ?>
+		'aPDF' => array('width' => 480, 'flexHeight' => true, 'resizeType' => 's'),		
+))) ?>
 
 <?php a_area('blog-sidebar', array(
   'editable' => false, 'toolbar' => 'basic', 'slug' => 'aBlog-'.$a_blog_post['id'],
-  'allowed_types' => array('aRichText', 'aImage', 'aButton', 'aSlideshow', 'aVideo'),
+  'allowed_types' => array('aRichText', 'aSlideshow', 'aVideo', 'aPDF'),
   'type_options' => array(
     'aRichText' => array('tool' => 'Main'),   
-    'aImage' => array('width' => 180, 'flexHeight' => true, 'resizeType' => 's', 'constraints' => array('minimum-width' => 180)),
-    'aButton' => array('width' => 180, 'flexHeight' => true, 'resizeType' => 's', 'constraints' => array('minimum-width' => 180)),
     'aSlideshow' => array("width" => 180, "flexHeight" => true, 'resizeType' => 's', 'constraints' => array('minimum-width' => 180)),
 		'aVideo' => array('width' => 180), 
-  ))
-) ?>
+		'aPDF' => array('width' => 180, 'flexHeight' => true, 'resizeType' => 's'),				
+))) ?>
