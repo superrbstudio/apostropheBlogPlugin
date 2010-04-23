c8c99965726461f89239578a2d1a275a36a0534f
dordille <dordille@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-23 16:08:20 +0000

    TH sort dropdowns in admin theme now have a selected class applied currently selected filters.  Fixes #178.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1023 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- data/generator/sfDoctrineModule/aBlogAdmin/template/templates/_list_th_dropdown.php
+++ data/generator/sfDoctrineModule/aBlogAdmin/template/templates/_list_th_dropdown.php
@@ -1,7 +1,12 @@
+[?php $values = $filters[$name]->getValue() ?]
+[?php $values = is_array($values)? $values : array($values) ?]
 <ul>
 	[?php foreach($filters[$name]->getWidget()->getChoices() as $id => $choice): ?]
 	[?php if($choice != ''): ?]
-	<li>[?php echo link_to($choice, 'aBlogAdmin/addFilter?name='.$name.'&value='.$id, 'post=true') ?]</li>
+  [?php $class = in_array($id, $values)? 'selected' : '' ?]
+	<li class="[?php echo $class ?]">
+    [?php echo link_to($choice, 'aBlogAdmin/addFilter?name='.$name.'&value='.$id, 'post=true') ?]
+  </li>
 	[?php endif ?]
 	[?php endforeach ?]
 </ul>
