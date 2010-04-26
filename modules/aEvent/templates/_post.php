e77b9891ff219856441f9bee475e679862ba46f8
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-26 20:41:12 +0000

    h2 -> h3
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1064 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aEvent/templates/_post.php
+++ modules/aEvent/templates/_post.php
@@ -1,8 +1,8 @@
 <?php use_helper('a') ?>
 <div class="a-blog-post">
-  <h2 class="a-blog-post-title">
+  <h3 class="a-blog-post-title">
     <?php echo link_to($a_event->getTitle(), 'a_event_post', $a_event) ?>
-  </h2>
+  </h3>
   <ul class="a-blog-post-meta">
     <li class="date"><?php echo date('l F jS Y', strtotime($a_event->getPublishedAt())) ?></li>
     <li class="author">Posted By: <?php echo $a_event->getAuthor() ?></li>   
