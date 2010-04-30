e3e49c88725375b50dd74707f9097ac42b63f42c
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-30 21:30:33 +0000

    removed the Hello Monkey alert from formScripts
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1115 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlogAdmin/templates/_formScripts.php
+++ modules/aBlogAdmin/templates/_formScripts.php
@@ -11,10 +11,6 @@
 				}
 	    });
 
-			$('#<?php echo $form['published_at']->renderId(); ?>').datepicker('option', 'onClose', function() {	
-					alert('hello monkey');
-				});
-
 			// Sidebar Toggle
 			// =============================================
 	    $('.a-sidebar-toggle').click(function(){
