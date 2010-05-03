ec16883adffae4367f367bdff2a96f1fb4d7fcf0
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-05-03 20:56:35 +0000

    editView for aEventsSlot
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1137 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/form/aEventSlotForm.class.php
+++ lib/form/aEventSlotForm.class.php
@@ -15,15 +15,19 @@ class aEventSlotForm extends sfForm
     
     $this->widgetSchema['count'] = new sfWidgetFormInput(array(), array('size' => 2));
     $this->validatorSchema['count'] = new sfValidatorNumber(array('min' => 0, 'max' => 10));
-    
+		$this->widgetSchema->setHelp('count', '<span class="a-help-arrow"></span> Set the number of posts to display – 10 max.');    
+		$this->setDefault('count', 1);
+		
     $this->widgetSchema['categories_list'] =
       new sfWidgetFormDoctrineChoice(array('multiple' => true, 'model' => 'aBlogCategory'));
     $this->validatorSchema['categories_list'] =
       new sfValidatorDoctrineChoice(array('model' => 'aBlogCategory', 'required' => false));
-    
+		$this->widgetSchema->setHelp('categories_list', '<span class="a-help-arrow"></span> Filter Events by Category');
+		    
     $this->widgetSchema['tags_list']       = new sfWidgetFormInput(array(), array('class' => 'tag-input', 'autocomplete' => 'off'));
     $this->validatorSchema['tags_list']    = new sfValidatorString(array('required' => false));
-        
+		$this->widgetSchema->setHelp('tags_list','<span class="a-help-arrow"></span> Filter Events by Tag');
+		        
     // Ensures unique IDs throughout the page
     $this->widgetSchema->setNameFormat('slot-form-' . $this->id . '[%s]');
     
