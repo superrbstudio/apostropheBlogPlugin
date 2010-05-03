3ff4cb52d5b6a2ec7f688f48fdd430d6f7af89c3
hillerj <hillerj@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-05-03 21:41:13 +0000

    basic two column template for blog slot updated to look the same as the single column
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1143 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_twoColumnTemplate_slot.php
+++ modules/aBlog/templates/_twoColumnTemplate_slot.php
@@ -1,7 +1,3 @@
-<div class="a-blog-item-excerpt">
-<?php echo $aBlogPost->getTextForArea('blog-body', $options['excerptLength']) ?>
-</div>
-
 <?php if($options['maxImages'] > 0): ?>
 <div class="a-blog-item-media">
 <?php include_component('aSlideshowSlot', 'slideshow', array(
@@ -12,3 +8,6 @@
 </div>
 <?php endif ?>
 
+<div class="a-blog-item-excerpt">
+<?php echo $aBlogPost->getTextForArea('blog-body', $options['excerptLength']) ?>
+</div>
