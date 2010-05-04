d1c122fe7f9571146ca61293e19e6445798f0be3
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-05-04 21:54:02 +0000

    aDate::pretty
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1176 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_post.php
+++ modules/aBlog/templates/_post.php
@@ -5,7 +5,7 @@
     <?php echo link_to($a_blog_post->getTitle(), 'a_blog_post', $a_blog_post) ?>
   </h3>
   <ul class="a-blog-item-meta">
-    <li class="date"><?php echo date('l F jS Y', strtotime($a_blog_post->getPublishedAt())) ?></li>
+    <li class="date">hello <?php echo aDate::pretty($a_blog_post['published_at']); ?></li>
     <li class="author"><?php echo __('Posted By:', array(), 'apostrophe_blog') ?> <?php echo $a_blog_post->getAuthor() ?></li>   
   </ul>
 <?php include_partial('aBlog/'.$a_blog_post->getTemplate(), array('a_blog_post' => $a_blog_post)) ?>
