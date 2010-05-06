c332f504f3d67dbb2abbae1e56b0851813fa22ba
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-05-06 21:06:38 +0000

    variable name bug
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1226 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aEvent/templates/_singleColumnTemplate_singleSlot.php
+++ modules/aEvent/templates/_singleColumnTemplate_singleSlot.php
@@ -1,3 +1,3 @@
 <?php // By Default, we just want the single slot to use the same markup as the multiple-slot ?>
 <?php // But by having it pull that slot in as a partial, we have the flexibility of overriding either template separately at the project level ?>
-<?php include_partial('aBlog/'.$aBlogItem['template'].'_slot', array('aBlogPost' => $aBlogItem, 'options' => $options)) ?>
+<?php include_partial('aEvent/'.$aEvent['template'].'_slot', array('aEvent' => $aEvent, 'options' => $options)) ?>
