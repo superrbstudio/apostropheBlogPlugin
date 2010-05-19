7fda9e6b23a8e7a43260f87c720b0e662bd6adf4
rickybanister <rickybanister@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-05-19 18:37:32 +0000

    moved the post meta data out of _post and into the post Templates with an if wrapper excluding it from the admin module
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1340 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_bannerTemplate_rss.php
+++
@@ -1,9 +0,0 @@
-<?php echo link_to($aBlogPost['title'], 'a_blog_post', $aBlogPost) ?> by <?php echo $aBlogPost->Author ?>
-<br/>
-<?php echo $aBlogPost['published_at'] ?>
-<br/><br/>
-<?php foreach($aBlogPost->Page->getArea('blog-body') as $slot): ?>
-<?php if(method_exists($slot, 'getSearchText')): ?>
-<?php echo $slot->getSearchText() ?>
-<?php endif ?>
-<?php endforeach ?>
--- modules/aEvent/templates/_singleColumnTemplate.php
+++ modules/aEvent/templates/_singleColumnTemplate.php
@@ -1,3 +1,13 @@
+<?php if ($sf_params->get('module') != 'aEventAdmin'): ?>
+<h3 class="a-blog-item-title">
+  <?php echo link_to($a_blog_post->getTitle(), 'a_blog_post', $a_blog_post) ?>
+</h3>
+<ul class="a-blog-item-meta">
+  <li class="date"><?php echo aDate::pretty($a_blog_post['published_at']); ?></li>
+  <li class="author"><?php echo __('Posted By:', array(), 'apostrophe_blog') ?> <?php echo $a_blog_post->getAuthor() ?></li>   
+</ul>
+<?php endif ?>
+
 <?php a_area('blog-body', array(
   'editable' => false, 'toolbar' => 'basic', 'slug' => $a_event->Page->slug,
   'allowed_types' => array('aRichText', 'aSlideshow', 'aVideo', 'aPDF'),
