49e42419fda1666346925608def9db956f46e218
dordille <dordille@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-06-30 15:43:50 +0000

    Merged fixes for seperating routing from actions.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1652 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/actions/BaseaBlogActions.class.php
+++ lib/actions/BaseaBlogActions.class.php
@@ -2,7 +2,7 @@
 
 /**
  * Base actions for the aBlogPlugin aBlog module.
- * 
+ *
  * @package     aBlogPlugin
  * @subpackage  aBlog
  * @author      P'unk Avenue
@@ -11,7 +11,7 @@
 abstract class BaseaBlogActions extends aEngineActions
 {
   protected $modelClass = 'aBlogPost';
-  
+
   public function preExecute()
   {
     parent::preExecute();
@@ -38,21 +38,25 @@ abstract class BaseaBlogActions extends aEngineActions
       $q->whereIn('c.id', $categoryIds);
     }
 
-    $routingOptions = $this->getRoute()->getOptions();
-    if(isset($routingOptions['filters']))
+    if($request->hasParameter('year') || $request->hasParameter('month') || $request->hasParameter('day'))
     {
-      foreach($routingOptions['filters'] as $method)
-      {
-        Doctrine::getTable($this->modelClass)->$method($q, $request);
-      }
-      $this->getResponse()->addMeta('robots', 'noarchive, nofollow');
+      Doctrine::getTable($this->modelClass)->filterByYMD($q, $request);
     }
+    if($request->hasParameter('cat'))
+    {
+      Doctrine::getTable($this->modelClass)->filterByCategory($q, $request);
+    }
+    if($request->hasParameter('tag'))
+    {
+      Doctrine::getTable($this->modelClass)->filterByTag($q, $request);
+    }
+
     Doctrine::getTable($this->modelClass)->addPublished($q);
     $q->orderBy('published_at desc');
-    
+
     return $q;
   }
-  
+
   public function executeIndex(sfWebRequest $request)
   {
     $this->buildParams();
@@ -60,7 +64,7 @@ abstract class BaseaBlogActions extends aEngineActions
     $pager->setQuery($this->buildQuery($request));
     $pager->setPage($this->getRequestParameter('page', 1));
     $pager->init();
-    
+
     $this->pager = $pager;
 
     aBlogItemTable::populatePages($pager->getResults());
@@ -70,9 +74,9 @@ abstract class BaseaBlogActions extends aEngineActions
       $this->getFeed();
       return sfView::NONE;
     }
-    
+
   }
-  
+
   public function executeShow(sfWebRequest $request)
   {
     $this->buildParams();
@@ -83,8 +87,8 @@ abstract class BaseaBlogActions extends aEngineActions
     $this->forward404Unless($this->aBlogPost['status'] == 'published');
     aBlogItemTable::populatePages(array($this->aBlogPost));
   }
-  
-  
+
+
   public function buildParams()
   {
     $this->params = array();
@@ -93,26 +97,26 @@ abstract class BaseaBlogActions extends aEngineActions
     $this->params['pagination']['year']  = $this->getRequestParameter('year');
     $this->params['pagination']['month'] = $this->getRequestParameter('month');
     $this->params['pagination']['day']   = $this->getRequestParameter('day');
-    
+
     $date = strtotime($this->getRequestParameter('year', date('Y')).'-'.$this->getRequestParameter('month', date('m')).'-'.$this->getRequestParameter('day', date('d')));
-    
+
     $this->dateRange = '';
     // set our parameters for building links that browse date ranges
     if ($this->getRequestParameter('day'))
     {
       $next = strtotime('tomorrow', $date);
       $this->params['next'] = array('year' => date('Y', $next), 'month' => date('m', $next), 'day' => date('d', $next));
-      
+
       $prev = strtotime('yesterday', $date);
       $this->params['prev'] = array('year' => date('Y', $prev), 'month' => date('m', $prev), 'day' => date('d', $prev));
-      
+
       $this->dateRange = 'day';
     }
     else if ($this->getRequestParameter('month'))
     {
       $next = strtotime('next month', $date);
       $this->params['next'] = array('year' => date('Y', $next), 'month' => date('m', $next));
-      
+
       $prev = strtotime('last month', $date);
       $this->params['prev'] = array('year' => date('Y', $prev), 'month' => date('m', $prev));
 
@@ -122,7 +126,7 @@ abstract class BaseaBlogActions extends aEngineActions
     {
       $next = strtotime('next year', $date);
       $this->params['next'] = array('year' => date('Y', $next));
-      
+
       $prev = strtotime('last year', $date);
       $this->params['prev'] = array('year' => date('Y', $prev));
 
@@ -131,18 +135,18 @@ abstract class BaseaBlogActions extends aEngineActions
         $this->dateRange = 'year';
       }
     }
-    
+
     // set our parameters for building links that set the date ranges
     $this->params['day'] = array('year' => date('Y', $date), 'month' => date('m', $date), 'day' => date('d', $date));
     $this->params['month'] = array('year' => date('Y', $date), 'month' => date('m', $date));
     $this->params['year'] = array('year' => date('Y', $date));
     $this->params['nodate'] = array();
-    
+
     $this->addFilterParams('cat');
     $this->addFilterParams('tag');
     $this->addFilterParams('search');
   }
-  
+
   public function addFilterParams($name)
   {
     // if there is a filter request, we need to add it to our date params
@@ -153,19 +157,19 @@ abstract class BaseaBlogActions extends aEngineActions
         $params[$name] = $this->getRequestParameter($name);
       }
     }
-    
+
     // set an array for building a link to this filter (we don't want it to already have the filter in there)
     $this->params[$name] = $this->params['pagination'];
     unset($this->params[$name][$name]);
   }
-  
+
   public function getFeed()
   {
     $this->articles = $this->pager->getResults();
     aBlogItemTable::populatePages($this->articles);
-    
+
     $title = sfConfig::get('app_aBlog_feed_title', $this->page->getTitle());
-    
+
     $this->feed = sfFeedPeer::createFromObjects(
       $this->articles,
       array(
@@ -178,7 +182,7 @@ abstract class BaseaBlogActions extends aEngineActions
         'methods'     => array('description' => 'getFeedText')
       )
     );
-    
+
     $this->getResponse()->setContent($this->feed->asXml());
   }
 }
