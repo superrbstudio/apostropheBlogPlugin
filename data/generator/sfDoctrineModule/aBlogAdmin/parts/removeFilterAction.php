0733b014bfeac233d4b8bc7ffda408d8e6fd6e97
dordille <dordille@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-07-02 20:15:18 +0000

    Using unset to clear filters instead of setting them to null.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.4@1694 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- data/generator/sfDoctrineModule/aBlogAdmin/parts/removeFilterAction.php
+++ data/generator/sfDoctrineModule/aBlogAdmin/parts/removeFilterAction.php
@@ -4,7 +4,7 @@
     $value = $request->getParameter('value');
     
     $filters = $this->getUser()->getAttribute('<?php echo $this->getModuleName() ?>.filters', $this->configuration->getFilterDefaults(), 'admin_module');
-    $filters[$name] = null;
+    unset($filters[$name]);
     $this->getUser()->setAttribute('<?php echo $this->getModuleName() ?>.filters', $filters, 'admin_module');
     
     $this->redirect('@<?php echo $this->getUrlForAction('list') ?>');
