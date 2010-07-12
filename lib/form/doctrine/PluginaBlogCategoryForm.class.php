c24792f41fd0d9d226f2adcc7786cace1e04493e
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-07-12 20:37:19 +0000

    Being a user for a category means that you can categorize your posts into it. It does not mean that you can edit other people's posts that happen to be in it. Only blog admins have blanket permission to do the latter.
    You must be a member of the editors group (if there is one) to be listed as a category author. This limits the size of the dropdown and is necessary for the same reasons as in the page settings dialog.
    
    Added an overridable getUseFields method to aBlogCategoryForm::setup() making it possible to add fields without getting clobbered by the useFields() call in setup().
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1735 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/form/doctrine/PluginaBlogCategoryForm.class.php
+++ lib/form/doctrine/PluginaBlogCategoryForm.class.php
@@ -10,11 +10,26 @@
  */
 abstract class PluginaBlogCategoryForm extends BaseaBlogCategoryForm
 {
+  protected function getUseFields()
+  {
+    return array('name', 'users_list', 'posts', 'events');
+  }
   public function setup()
   {
-    parent::setup();
-    
-    $this->useFields(array('name', 'users_list', 'posts', 'events'));
+    parent::setup();    
+    $this->useFields($this->getUseFields());
+    $q = Doctrine::getTable('sfGuardUser')->createQuery();
+    // You must be a member of the editor group to potentially post in a category.
+    // Listing all users produces an unmanageable dropdown
+    $candidateGroup = sfConfig::get('app_a_edit_candidate_group', false);
+    if ($candidateGroup)
+    {
+      $q->innerJoin('sfGuardUser.groups g')->addWhere('g.name = ?', array($candidateGroup));
+    }
+    $this->setWidget('users_list',
+      new sfWidgetFormDoctrineChoice(array('multiple' => true, 'model' => 'sfGuardUser', 'query' => $q)));
+    $this->setValidator('users_list',
+      new sfValidatorDoctrineChoice(array('multiple' => true, 'model' => 'sfGuardUser', 'query' => $q, 'required' => false)));
   }
 
 }
