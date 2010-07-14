33e8a6befcfc619c1f50dc55fd234a04b41a2d6e
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-07-14 16:10:50 +0000

    cleanup of tags, fixed categories bug where the permissions check was not wrapped around the edit button
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1750 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aEventAdmin/templates/_formScripts.php
+++ modules/aEventAdmin/templates/_formScripts.php
@@ -37,6 +37,7 @@
 	    aBlogPublishBtn('<?php echo $a_event->status  ?>','<?php echo url_for('a_event_admin_update',$a_event) ?>');
 	    aMultipleSelect('#categories-section', { 'choose-one': '<?php echo __('Choose Categories', array(), 'apostrophe_blog') ?>' <?php if($sf_user->hasCredential('admin')): ?>, 'add': '<?php echo __('+ New Category', array(), 'apostrophe_blog') ?>'<?php endif ?>, 'onChange': aBlogUpdateMulti });
 	    aMultipleSelect('#editors-section', { 'choose-one': '<?php echo __('Choose Editors', array(), 'apostrophe_blog') ?>','onChange': aBlogUpdateMulti  });
+	
 	 });
 	$(window).bind('beforeunload', function() {
 	<?php // We want to save the blog post editor when you close the browser window or navigate away from it ?>
