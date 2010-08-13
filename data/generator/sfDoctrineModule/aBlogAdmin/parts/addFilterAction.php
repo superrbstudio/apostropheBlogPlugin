1a724f2498fb14354bb24a5809ac5d52bb727a82
dordille <dordille@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-08-13 19:05:16 +0000

    Applied [1947] to trunk
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@1948 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- data/generator/sfDoctrineModule/aBlogAdmin/parts/addFilterAction.php
+++ data/generator/sfDoctrineModule/aBlogAdmin/parts/addFilterAction.php
@@ -2,12 +2,12 @@
   {
     $name = $request->getParameter('name');
     $value = $request->getParameter('value');
-    
+
     $filters = $this->getUser()->getAttribute('<?php echo $this->getModuleName() ?>.filters', $this->configuration->getFilterDefaults(), 'admin_module');
-    
+
     $filters[$name] = $value;
     $this->getUser()->setAttribute('<?php echo $this->getModuleName() ?>.filters', $filters, 'admin_module');
-    
-    $this->redirect('@<?php echo $this->getUrlForAction('list') ?>');    
+    $this->setPage(1);
+    $this->redirect('@<?php echo $this->getUrlForAction('list') ?>');
   }
     
