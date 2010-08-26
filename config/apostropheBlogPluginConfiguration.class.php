20ebc0bed9e27f85e083d45f36503355a5763b0f
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-08-26 18:34:36 +0000

    updated events button to use the new button markup
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@2040 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- config/apostropheBlogPluginConfiguration.class.php
+++ config/apostropheBlogPluginConfiguration.class.php
@@ -39,7 +39,7 @@ class apostropheBlogPluginConfiguration extends sfPluginConfiguration
     {
       aTools::addGlobalButtons(array(
         new aGlobalButton('blog', 'Blog', '@a_blog_admin', 'a-blog-btn'),
-        new aGlobalButton('events', '<span class="day"></span> Events', '@a_event_admin', 'a-events day-'.date('j'))
+        new aGlobalButton('events', 'Events', '@a_event_admin', 'a-events day-'.date('j'))
       ));
     }
   }
