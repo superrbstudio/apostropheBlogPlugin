fcde22ea9be3f284bf5f8276fb1691d7dee8fe16
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-08-27 15:56:20 +0000

    blog heading indexSuccess
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.4@2068 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/indexSuccess.php
+++ modules/aBlog/templates/indexSuccess.php
@@ -18,11 +18,13 @@
 
 <div id="a-blog-main" class="a-blog-main">
   <?php if ($sf_params->get('year')): ?>
-  <h2><?php echo $sf_params->get('day') ?> <?php echo ($sf_params->get('month')) ? date('F', strtotime(date('Y').'-'.$sf_params->get('month').'-01')) : '' ?> <?php echo $sf_params->get('year') ?></h2>
-  <ul class="a-controls a-blog-browser-controls">
-    <li><?php echo link_to('Previous', 'aBlog/index?'.http_build_query($params['prev']), array('class' => 'a-arrow-btn icon a-arrow-left', )) ?></li>
-    <li><?php echo link_to('Next', 'aBlog/index?'.http_build_query($params['next']), array('class' => 'a-arrow-btn icon a-arrow-right', )) ?></li>
-  </ul>
+	<div class="a-blog-heading">
+	  <h3><?php echo $sf_params->get('day') ?> <?php echo ($sf_params->get('month')) ? date('F', strtotime(date('Y').'-'.$sf_params->get('month').'-01')) : '' ?> <?php echo $sf_params->get('year') ?></h3>
+	  <ul class="a-controls a-blog-browser-controls">
+	    <li><?php echo link_to('Previous', 'aBlog/index?'.http_build_query($params['prev']), array('class' => 'a-arrow-btn icon a-arrow-left', )) ?></li>
+	    <li><?php echo link_to('Next', 'aBlog/index?'.http_build_query($params['next']), array('class' => 'a-arrow-btn icon a-arrow-right', )) ?></li>
+	  </ul>
+	</div>
   <?php endif ?>
   
   <?php if($sf_user->isAuthenticated()): ?>
