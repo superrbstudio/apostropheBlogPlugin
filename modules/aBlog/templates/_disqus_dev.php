c91b98f27a939e9387169fa0982048fe86163a43
jake <jake@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-10-05 17:49:45 +0000

    removed the disqus dev-mode partial. cleaned up some of the code in the other disqus stuff.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@2338 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_disqus_dev.php
+++
@@ -1,5 +0,0 @@
-<?php  if (sfConfig::get('app_aBlog_disqus_devmode', true)):?>
-<script type="text/javascript"> 
-  var disqus_developer = 1; // this would set it to developer mode 
-</script>
-<?php endif ?>
