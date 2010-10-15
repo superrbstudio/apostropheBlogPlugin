eb957bdf2e09010c3e70d824f891435c07d01223
jake <jake@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-10-15 18:47:31 +0000

    fixed a bug with disqus comments. button was showing up when disqus was disabled.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@2429 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlogAdmin/templates/_list_actions.php
+++ modules/aBlogAdmin/templates/_list_actions.php
@@ -1,4 +1,4 @@
 <li class="a-admin-action-new"><?php echo link_to('<span class="icon"></span>'.__('New Post', array(), 'messages'), 'aBlogAdmin/new', array(  'class' => 'a-btn big icon a-add',)) ?></li>
-<?php  if (sfConfig::get('app_aBlog_disqus_enabled', true)):?>
-<li><?php echo link_to('Comments', "http://<?php echo(sfConfig::get('app_aBlog_disqus_shortname'))?>.disqus.com", array('class' => 'a-btn big', )) ?></li>
+<?php if (sfConfig::get('app_aBlog_disqus_enabled', true) && sfConfig::get('app_aBlog_disqus_shortname')): ?>
+<li><?php echo link_to('Comments', 'http://'. sfConfig::get('app_aBlog_disqus_shortname') .'.disqus.com', array('class' => 'a-btn big', )) ?></li>
 <?php endif ?>
