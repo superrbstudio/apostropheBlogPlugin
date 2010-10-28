c73280d2538b7cc46e90901a9604ef2525669953
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-10-28 21:57:49 +0000

    Geoff and I turned off all of the little save buttons in favor of just using the big one at the top.
    We should display a message if there are unsaved changes
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@2554 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aEventAdmin/templates/_form.php
+++ modules/aEventAdmin/templates/_form.php
@@ -29,12 +29,12 @@
 	
 	<div class="a-form-row">
 		<a href="#" class="a-btn big a-publish-post <?php echo ($a_event['status'] == 'published')? 'published':'' ?>" onclick="return false;" id="a-blog-publish-button">
-	  	<span class="publish"><?php echo __('Publish', array(), 'apostrophe') ?></span>
-	  	<span class="unpublish"><?php echo __('Unpublish', array(), 'apostrophe') ?></span>
+	  	<span class="publish"><?php echo a_('Publish') ?></span>
+	  	<span class="unpublish"><?php echo a_('Unpublish') ?></span>
 		</a>
 	
 		<a href="#" class="a-btn big a-save-post" id="a-event-save-button">
-	  	<span class="publish"><?php echo __('Save', array(), 'apostrophe') ?></span>
+	  	<span class="publish"><?php echo a_('Save') ?></span>
 		</a>
 	</div>
 
@@ -47,31 +47,28 @@
 	<div class="post-published <?php echo ($a_event['status'] != 'published')? 'draft':'published' ?>">
 		<div class="post-published-sentence-toggle">
 			<span class="draft">
-				<?php echo __('Scheduled for '.aDate::medium(strtotime($a_event['published_at'])), array(), 'apostrophe_blog') ?><br/>
-				<a href="#" onclick="return false;" class="post-date-toggle"><?php echo __('change date', array(), 'apostrophe_blog') ?></a>
+				<?php echo a_('Scheduled for '.aDate::medium(strtotime($a_event['published_at']))) ?><br/>
+				<a href="#" onclick="return false;" class="post-date-toggle"><?php echo a_('change date') ?></a>
 			</span>		
 			<span class="published">
-				<?php echo __('Published on '.aDate::medium(strtotime($a_event['published_at'])), array(), 'apostrophe_blog') ?><br/>
-				<a href="#" onclick="return false;" class="post-date-toggle"><?php echo __('change date', array(), 'apostrophe_blog') ?></a>		
+				<?php echo a_('Published on '.aDate::medium(strtotime($a_event['published_at']))) ?><br/>
+				<a href="#" onclick="return false;" class="post-date-toggle"><?php echo a_('change date') ?></a>		
 			</span>
 		</div>
+		
 		<div class="post-published-at option">
 		  <?php echo $form['published_at']->render(array('onClose' => 'aBlogUpdateMulti')) ?>
-		  <?php echo $form['published_at']->renderError() ?>
+		  <?php echo $form['published_at']->renderError() ?>		
+			<ul class="a-ui a-controls">
+				<li>
+	     		<?php echo a_js_button(a_('Save'), array('a-save', 'a-save-event', 'post-date-published-at-save-link', 'mini')) ?>							
+				</li>
+				<li>
+	      	<?php echo a_js_button(a_('Cancel'), array('a-cancel','icon','post-date-published-at-cancel', 'mini')) ?>
+				</li>
+			</ul>
 		</div>
-		
-		<div class="post-published-at option">
-			<span class="draft">
-				<a href="#" onclick="return false;" class="post-date-published-at-cancel"><?php echo __('Cancel', array(), 'apostrophe_blog') ?></a>
-			</span>		
-		</div>
-		
-		<div class="post-published-at option">
-			<a href="#" class="a-save-event post-date-published-at-save-link">
-	  		<span class="publish"><?php echo __('Save', array(), 'apostrophe') ?></span>
-			</a>
-		</div>
-		
+				
 	</div>	
 </div>
 
@@ -84,9 +81,15 @@
 
 	<div class="start_date">
 		<h4>Start Date</h4>
+		<div class="a-form-row all_day">
+			<div class="a-form-field">
+				<?php echo $form['all_day']->render() ?>
+			</div>
+			<?php echo $form['all_day']->renderLabel() ?>
+			<?php echo $form['all_day']->renderError() ?>
+		</div>		
 		<?php echo $form['start_date']->render(array('beforeShow' => 'aBlogSetDateRange', 'onClose' => 'aBlogUpdateMulti')) ?>
 		<?php echo $form['start_date']->renderError() ?>
-		
 		<div class="start_time">
     	<?php echo $form['start_time'] ?>
     	<?php echo $form['start_time']->renderError() ?>
@@ -97,27 +100,20 @@
 		<h4>End Date</h4>
 		<?php echo $form['end_date']->render(array('beforeShow' => 'aBlogSetDateRange', 'onClose' => 'aBlogUpdateMulti')) ?>
 		<?php echo $form['end_date']->renderError() ?>
-
 		<div class="end_time">
     	<?php echo $form['end_time'] ?>
     	<?php echo $form['end_time']->renderError() ?>
 		</div>
-		
 	</div>
 
-	<div class="all_day">
-		<h4>All Day Event</h4>
-		<?php echo $form['all_day']->renderLabel() ?>
-		<?php echo $form['all_day']->render() ?>
-		<?php echo $form['all_day']->renderError() ?>
-	</div>
+
 	
+	<?php if (0): ?>
 	<div class="post-event-times">
-		<a href="#" class="a-save-event">
-  		<span class="publish"><?php echo __('Save', array(), 'apostrophe') ?></span>
-		</a>
+ 		<?php  echo a_js_button(a_('Save'), array('a-save', 'a-save-event', 'mini')) ?>							
 	</div>
-	
+	<?php endif ?>
+		
 </div>
 
 
@@ -128,7 +124,7 @@
 
 	<?php // Blog Post Author ?>
 	<div class="post-author">
-	  	<h4><?php echo __('Author', array(), 'apostrophe') ?>
+	  	<h4><?php echo a_('Author') ?>
 			<?php if ($sf_user->hasCredential('admin')): ?>
 				</h4>
 				<div class="author_id option">
@@ -144,14 +140,14 @@
 	<div class="post-editors">
 
 		<?php if (!count($a_event->Editors)): ?>
-		  <a href="#" onclick="return false;" class="post-editors-toggle a-sidebar-toggle"><?php echo __('allow others to edit this post', array(), 'apostrophe') ?></a>
+		  <a href="#" onclick="return false;" class="post-editors-toggle a-sidebar-toggle"><?php echo a_('allow others to edit this post') ?></a>
 	  	<div class="post-editors-options option" id="editors-section">
 		<?php else: ?>
 			<hr/>
 	  	<div class="post-editors-options option show-editors" id="editors-section">
 		<?php endif ?>
 
-	    <h4><?php echo __('Editors', array(), 'apostrophe') ?></h4>
+	    <h4><?php echo a_('Editors') ?></h4>
 	    <?php echo $form['editors_list']->render()?>
 	    <?php echo $form['editors_list']->renderError() ?>
 
@@ -164,7 +160,7 @@
 	<?php if(isset($form['template'])): ?>
 	<hr />
 	<div class="template section">
-		<h4><?php echo __('Template', array(), 'apostrophe') ?></h4>
+		<h4><?php echo a_('Template') ?></h4>
 		<?php echo $form['template']->render() ?>
 		<?php echo $form['template']->renderError() ?>
 	</div>
@@ -175,7 +171,7 @@
 	<?php if(isset($form['allow_comments'])): ?>
 	<hr />
 	<div class="comments section">
-		<h4><a href="#" class="allow_comments_toggle <?php echo ($a_event['allow_comments'])? 'enabled' : 'disabled' ?>"><span class="enabled" title="<?php echo __('Click to disable comments', array(), 'apostrophe') ?>"><?php echo __('Comments are enabled', array(), 'apostrophe') ?></span><span class="disabled" title="<?php echo __('Click to enable comments', array(), 'apostrophe') ?>"><?php echo __('Comments are disabled', array(), 'apostrophe') ?></span></a></h4>
+		<h4><a href="#" class="allow_comments_toggle <?php echo ($a_event['allow_comments'])? 'enabled' : 'disabled' ?>"><span class="enabled" title="<?php echo a_('Click to disable comments') ?>"><?php echo a_('Comments are enabled') ?></span><span class="disabled" title="<?php echo a_('Click to enable comments') ?>"><?php echo a_('Comments are disabled') ?></span></a></h4>
 		<div class="allow_comments option">
 			<?php echo $form['allow_comments']->render() ?>
 			<?php echo $form['allow_comments']->renderError() ?>
@@ -183,27 +179,28 @@
 	</div>
 	<?php endif ?>
 	
-	<div class="post-author-section">
-		<a href="#" class="a-save-event">
-  		<span class="publish"><?php echo __('Save', array(), 'apostrophe') ?></span>
-		</a>
+	<?php if (0): ?>
+	<div class="post-author">
+ 		<?php  echo a_js_button(a_('Save'), array('a-save', 'a-save-event', 'mini')) ?>							
 	</div>
-
+	<?php endif ?>
+	
 	<?php // Blog Post Categories ?>
 	<hr />
 	<div class="categories section a-form-row" id="categories-section">
-		<h4><?php echo __('Categories', array(), 'apostrophe') ?></h4>
+		<h4><?php echo a_('Categories') ?></h4>
 		<?php if($sf_user->hasCredential('admin')): ?>
 			<?php echo link_to('<span class="icon"></span>'.a_('edit categories'),'@a_category_admin', array('class' => 'a-btn icon a-edit no-label lite edit-categories', 'title' => a_('edit categories'))) ?>
 	  <?php endif ?>
 		<?php echo $form['categories_list']->render() ?>
 		<?php echo $form['categories_list']->renderError() ?>
 		
+		<?php if (0): ?>
 		<div class="post-categories-section">
-			<a href="#" class="a-save-event">
-	  		<span class="publish"><?php echo __('Save', array(), 'apostrophe') ?></span>
-			</a>
+	 		<?php echo a_js_button(a_('Save'), array('a-save', 'a-save-event', 'mini')) ?>							
 		</div>
+		<?php endif ?>
+				
 	</div>
 
 	<?php // Blog Post Tags ?>
@@ -211,13 +208,14 @@
 	<div class="tags section a-form-row">
 		<?php echo $form['tags']->render() ?>
 		<?php echo $form['tags']->renderError() ?>
-		<?php a_js_call('pkInlineTaggableWidget(?, ?)', '#a-blog-post-tags-input', array('popular-tags' => $popularTags, 'existing-tags' => $existingTags, 'typeahead-url' => url_for('taggableComplete/complete'), 'tagsLabel' => 'Tags')) ?>
+		<?php a_js_call('pkInlineTaggableWidget(?, ?)', '#a-blog-post-tags-input', array('popular-tags' => $popularTags, 'existing-tags' => $existingTags, 'typeahead-url' => url_for('taggableComplete/complete'), 'tags-label' => a_('Tags'))) ?>
 		
+		<?php if (0): ?>
 		<div class="post-tags-section">
-			<a href="#" class="a-save-event">
-	  		<span class="publish"><?php echo __('Save', array(), 'apostrophe') ?></span>
-			</a>
+	 		<?php echo a_js_button(a_('Save'), array('a-save', 'a-save-event', 'mini')) ?>							
 		</div>
+		<?php endif ?>
+				
 	</div>
 
 	<?php if($a_event->userHasPrivilege('delete')): ?>
@@ -225,8 +223,8 @@
 		<div class="delete preview section a-form-row">
 			<?php $engine = $a_event->findBestEngine(); ?>
 	    <?php aRouteTools::pushTargetEnginePage($engine) ?>
-			<?php echo link_to('<span class="icon"></span>'.__('Preview', array(), 'apostrophe'), 'a_blog_post', array('preview' => 1) + $a_event->getRoutingParams(), array('class' => 'a-btn icon a-search lite a-align-left', 'rel' => 'external')) ?>
-			<?php echo link_to('<span class="icon"></span>'.__('Delete', array(), 'apostrophe'), 'a_event_admin_delete', $a_event, array('class' => 'a-btn icon a-delete lite a-align-right', 'method' => 'delete', 'confirm' => __('Are you sure you want to delete this event?', array(), 'apostrophe'), )) ?>
+			<?php echo link_to('<span class="icon"></span>'.a_('Preview'), 'a_blog_post', array('preview' => 1) + $a_event->getRoutingParams(), array('class' => 'a-btn icon a-search lite a-align-left', 'rel' => 'external')) ?>
+			<?php echo link_to('<span class="icon"></span>'.a_('Delete'), 'a_event_admin_delete', $a_event, array('class' => 'a-btn icon a-delete lite a-align-right', 'method' => 'delete', 'confirm' => a_('Are you sure you want to delete this event?'), )) ?>
 		</div>
 	<?php endif ?>
 
