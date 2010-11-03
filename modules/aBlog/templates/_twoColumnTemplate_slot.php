5c91a643afbe22b78dac54aa42f664f67bce23e7
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-11-03 14:25:16 +0000

    Fixes #694
    Catch routing exceptions and display a helpful "you must have a blog page / events page" message rather than crashing the home page if you have an event/blog slot there and the only blog/event page has been removed. (This happens because permalinks can't be generated. Down the road we can think about whether to support using the blog with no permalinks - strictly content repository only mode - but for now let's just not crash and be helpful and informative)
    
    Yes, try/catch in a template is unusual, but it's appropriate here. Down the road we might consider dumbing down these partials more and generating the URLs in the component, catching exceptions etc. there
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@2596 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_twoColumnTemplate_slot.php
+++ modules/aBlog/templates/_twoColumnTemplate_slot.php
@@ -3,6 +3,12 @@
   $aBlogPost = isset($aBlogPost) ? $sf_data->getRaw('aBlogPost') : null;
   $options = isset($options) ? $sf_data->getRaw('options') : null;
 ?>
+<?php // Yes, catching exceptions in templates is unusual, but if there is no blog page on ?>
+<?php // the site it is not possible to generate some of the links. That can kill the home page, ?>
+<?php // so we must address it. Someday it might be better to do less in the template and ?>
+<?php // generate the various URLs in component code rather than partial code ?>
+<?php try { ?>
+
 <h3 class="a-blog-item-title"><?php echo link_to($aBlogPost['title'], 'a_blog_post', $aBlogPost) ?></h3>
 
 <ul class="a-blog-item-meta">
@@ -28,3 +34,7 @@
     <?php echo link_to('Read More', 'a_blog_post', $aBlogPost, array('class' => 'a-blog-more')) ?>
   </div>
 </div>
+
+<?php } catch (Exception $e) { ?>
+  <h3>You must have a blog page somewhere on your site to use blog slots.</h3>
+<?php } ?>
