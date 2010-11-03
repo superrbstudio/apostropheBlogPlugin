f793e4f4128ca46985fa1bc33a320d3623b7e0ec
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-11-03 14:28:22 +0000

    Resolves #694, you can still use the home page if you delete your blog page and the home page contains a blog slot
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.4@2597 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_singleColumnTemplate_slot.php
+++ modules/aBlog/templates/_singleColumnTemplate_slot.php
@@ -1,25 +1,35 @@
-<h3 class="a-blog-item-title"><?php echo link_to($aBlogPost['title'], 'a_blog_post', $aBlogPost) ?></h3>
+<?php // Yes, catching exceptions in templates is unusual, but if there is no blog page on ?>
+<?php // the site it is not possible to generate some of the links. That can kill the home page, ?>
+<?php // so we must address it. Someday it might be better to do less in the template and ?>
+<?php // generate the various URLs in component code rather than partial code ?>
+<?php try { ?>
+  
+  <h3 class="a-blog-item-title"><?php echo link_to($aBlogPost['title'], 'a_blog_post', $aBlogPost) ?></h3>
 
-<ul class="a-blog-item-meta">
-	<li class="date"><?php echo aDate::long($aBlogPost['published_at']) ?></li>
-	<li class="author"><?php echo __('Posted By:', array(), 'apostrophe_blog') ?> <?php echo $aBlogPost->getAuthor() ?></li>   			
-</ul>
+  <ul class="a-blog-item-meta">
+  	<li class="date"><?php echo aDate::long($aBlogPost['published_at']) ?></li>
+  	<li class="author"><?php echo __('Posted By:', array(), 'apostrophe') ?> <?php echo $aBlogPost->getAuthor() ?></li>   			
+  </ul>
 
-<?php if($options['maxImages'] && $aBlogPost->hasMedia()): ?>
-	<div class="a-blog-item-media">
-	<?php include_component('aSlideshowSlot', 'slideshow', array(
-	  'items' => $aBlogPost->getMediaForArea('blog-body', 'image', $options['maxImages']),
-	  'id' => 'a-slideshow-blogitem-'.$aBlogPost['id'],
-	  'options' => $options['slideshowOptions']
-	  )) ?>
-	</div>
-<?php endif ?>
+  <?php if($options['maxImages'] && $aBlogPost->hasMedia()): ?>
+  	<div class="a-blog-item-media">
+  	<?php include_component('aSlideshowSlot', 'slideshow', array(
+  	  'items' => $aBlogPost->getMediaForArea('blog-body', 'image', $options['maxImages']),
+  	  'id' => 'a-slideshow-blogitem-'.$aBlogPost['id'],
+  	  'options' => $options['slideshowOptions']
+  	  )) ?>
+  	</div>
+  <?php endif ?>
 
-<div class="a-blog-item-excerpt-container">
-	<div class="a-blog-item-excerpt">
-		<?php echo $aBlogPost->getTextForArea('blog-body', $options['excerptLength']) ?>
-	</div>
-  <div class="a-blog-read-more">
-    <?php echo link_to('Read More', 'a_blog_post', $aBlogPost, array('class' => 'a-blog-more')) ?>
+  <div class="a-blog-item-excerpt-container">
+  	<div class="a-blog-item-excerpt">
+  		<?php echo $aBlogPost->getTextForArea('blog-body', $options['excerptLength']) ?>
+  	</div>
+    <div class="a-blog-read-more">
+      <?php echo link_to('Read More', 'a_blog_post', $aBlogPost, array('class' => 'a-blog-more')) ?>
+    </div>
   </div>
-</div>
+  
+<?php } catch (Exception $e) { ?>
+  <h3>You must have a blog page somewhere on your site to use blog slots.</h3>
+<?php } ?>
--- modules/aEvent/templates/_singleColumnTemplate_slot.php
+++ modules/aEvent/templates/_singleColumnTemplate_slot.php
@@ -1,3 +1,9 @@
+<?php // Yes, catching exceptions in templates is unusual, but if there is no blog page on ?>
+<?php // the site it is not possible to generate some of the links. That can kill the home page, ?>
+<?php // so we must address it. Someday it might be better to do less in the template and ?>
+<?php // generate the various URLs in component code rather than partial code ?>
+<?php try { ?>
+
 <h3 class="a-blog-item-title"><?php echo link_to($aEvent['title'], 'a_event_post', $aEvent) ?></h3>
 
 <?php include_partial('aEvent/meta', array('aEvent' => $aEvent)) ?>
@@ -20,3 +26,7 @@
 		<?php echo link_to('Read More', 'a_event_post', $aEvent, array('class' => 'a-blog-more')) ?>
 	</div>
 </div>
+
+<?php } catch (Exception $e) { ?>
+  <h3>You must have an events page somewhere on your site to use event slots.</h3>
+<?php } ?>
