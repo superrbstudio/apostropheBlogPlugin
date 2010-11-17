87d3946b402a33a1c809a2446b912bdf080275e5
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-11-17 23:09:58 +0000

    switched the form formatter for the engine page settings forms. they use aPageSettings formatter now. todo: document why this is so
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.5@2679 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/form/PluginaBlogEngineForm.class.php
+++ lib/form/PluginaBlogEngineForm.class.php
@@ -8,10 +8,11 @@ class PluginaBlogEngineForm extends aPageForm
 
     $this->useFields(array('categories_list'));
     $q = Doctrine::getTable('aBlogPost')->addCategories();
-    $this->widgetSchema->setLabel('categories_list', 'Categories');
+    $this->widgetSchema->setLabel('categories_list', 'Blog Categories');
     $this->widgetSchema->setHelp('categories_list','(Defaults to All Cateogories)');
     $this->getValidator('categories_list')->setOption('required', false);
     $this->widgetSchema->setNameFormat('enginesettings[%s]');
-    $this->widgetSchema->setFormFormatterName('aAdmin');
+		// We use the aPageSettings formatter here instead of aAdmin because it puts H4 tags around the labels for styling
+    $this->widgetSchema->setFormFormatterName('aPageSettings');
   }
 }
--- lib/form/PluginaEventEngineForm.class.php
+++ lib/form/PluginaEventEngineForm.class.php
@@ -9,10 +9,10 @@ class PluginaEventEngineForm extends aPageForm
     $this->useFields();
     $q = Doctrine::getTable('aEvent')->addCategories();
     $this->setWidget('blog_categories_list', new sfWidgetFormDoctrineChoice(array('multiple' => true, 'query' => $q, 'model' => 'aCategory')));
-    $this->widgetSchema->setLabel('blog_categories_list', 'Categories');
+    $this->widgetSchema->setLabel('blog_categories_list', 'Events Categories');
     $this->widgetSchema->setHelp('blog_categories_list','(Defaults to All Cateogories)');
     $this->setValidator('blog_categories_list', new sfValidatorDoctrineChoice(array('multiple' => true, 'query' => $q, 'model' => 'aCategory', 'required' => false)));
     $this->widgetSchema->setNameFormat('enginesettings[%s]');
-    $this->widgetSchema->setFormFormatterName('aAdmin');
+    $this->widgetSchema->setFormFormatterName('aPageSettings');
   }
 }
