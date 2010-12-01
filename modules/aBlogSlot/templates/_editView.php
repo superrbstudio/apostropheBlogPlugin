e453bf40469836b499a1b936c1a5fdabbe00c04e
jake <jake@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-12-01 18:53:48 +0000

    Styled the new combined single/filtered blog slot
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.5@2752 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlogSlot/templates/_editView.php
+++ modules/aBlogSlot/templates/_editView.php
@@ -7,23 +7,13 @@
 <?php echo $form->renderHiddenFields() ?>
 
 <h4 class="a-slot-form-title"><?php echo a_('Select Blog Posts') ?></h4>
-
-<div class="a-form-row by-type">
-  <?php $w = $form['title_or_tag'] ?>
-  <input type="radio" id="<?php echo $w->renderId() ?>-title" name="<?php echo $w->renderName() ?>" value="title" <?php echo ($w->getValue() === "title") ? 'checked' : '' ?> /> <label for="<?php echo $w->renderId() ?>-title"><?php echo a_('By Title') ?></label>
-
-  <div class="a-form-row blog-posts">
-    <?php echo $form['blog_posts']->render() ?>
-  </div>
-</div>
-
-<div class="a-form-row by-type">
+<div class="a-form-row by-type meta">
   <?php $w = $form['title_or_tag'] ?>
   <input type="radio" id="<?php echo $w->renderId() ?>-tag" name="<?php echo $w->renderName() ?>" value="tags" <?php echo ($w->getValue() === "tags") ? 'checked' : '' ?> /> <label for="<?php echo $w->renderId() ?>-tag"><?php echo a_('By Category and Tag') ?></label>
-
   <div class="a-form-row count">
   	<div class="a-form-field">
       <?php echo a_('Show %count% Post(s)', array('%count%' => $form['count']->render())) ?>
+				<div class="a-form-help collapsed"><?php echo $form['count']->renderHelp() ?></div>
   	</div>
   	<div class="a-form-error"><?php echo $form['count']->renderError() ?></div>
   </div>
@@ -31,7 +21,8 @@
   <div class="a-form-row categories">
     <div class="a-form-field">
       <label class="a-multiple-select-label" for="<?php echo $form['categories_list']->renderId() ?>"><?php echo a_('Categorized')?></label><?php echo $form['categories_list']->render() ?>
-    </div>
+    		<div class="a-form-help collapsed"><?php echo $form['categories_list']->renderHelp() ?></div>
+		</div>
   	<div class="a-form-error"><?php echo $form['categories_list']->renderError() ?></div>
   </div>
 
@@ -48,13 +39,32 @@
       <?php endif ?>
       <?php a_js_call('pkInlineTaggableWidget(?, ?)', '#' . $form['tags_list']->renderId(), $options) ?>
   	</div>
+		<div class="a-form-help collapsed"><?php echo $form['tags_list']->renderHelp() ?></div>
   	<div class="a-form-error"><?php echo $form['tags_list']->renderError() ?></div>
   </div>
 </div>
+<hr />
+<div class="a-form-row by-type title">
+  <?php $w = $form['title_or_tag'] ?>
+  <input type="radio" id="<?php echo $w->renderId() ?>-title" name="<?php echo $w->renderName() ?>" value="title" <?php echo ($w->getValue() === "title") ? 'checked' : '' ?> /> <label for="<?php echo $w->renderId() ?>-title"><?php echo a_('By Title') ?></label>
+
+  <div class="a-form-row blog-posts">
+    <?php echo $form['blog_posts']->render() ?>
+  </div>
+</div>
+
 
 <script type="text/javascript" charset="utf-8">
 $(document).ready(function() {
     aMultipleSelect('#a-<?php echo $form->getName() ?> .blog-posts', { 'autocomplete': <?php echo json_encode(url_for("aBlogAdmin/search")) ?> });
     aMultipleSelect('#a-<?php echo $form->getName() ?> .categories', { 'choose-one': 'Add Categories' });
+		var slotEditForm = $('#a-<?php echo $form->getName() ?>')
+		var editStates = slotEditForm.find('.a-form-row.by-type input[type="radio"]');
+		var editState = slotEditForm.find('.a-form-row.by-type input[type="radio"]:checked').val();
+		slotEditForm.addClass('a-options dropshadow editState-' + editState );
+			editStates.live('click', function(){
+			 	editState = slotEditForm.find('.a-form-row.by-type input[type="radio"]:checked').val();
+				slotEditForm.removeClass('editState-title').removeClass('editState-tags').addClass('editState-'+editState);
+			});
 	});
 </script>
