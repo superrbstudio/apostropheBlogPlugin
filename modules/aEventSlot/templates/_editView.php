52db82ab6dbdddb97ff1111baa24f2be5506aac2
jake <jake@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-12-21 21:58:24 +0000

    wrapped the blog & event edit views in a div to help clean up their styles
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/trunk@2930 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aEventSlot/templates/_editView.php
+++ modules/aEventSlot/templates/_editView.php
@@ -5,38 +5,54 @@
 <?php // Just echo the form. You might want to render the form fields differently ?>
 
 <?php echo $form->renderHiddenFields() ?>
+<div class="a-blog-edit-wrapper">
+	<h4 class="a-slot-form-title"><?php echo a_('Select Events') ?></h4>
+	<div class="a-form-row by-type meta">
+	  <?php $w = $form['title_or_tag'] ?>
+	  <input type="radio" id="<?php echo $w->renderId() ?>-tag" name="<?php echo $w->renderName() ?>" value="tags" <?php echo ($w->getValue() === "tags") ? 'checked' : '' ?> /> <label for="<?php echo $w->renderId() ?>-tag"><?php echo a_('By Category and Tag') ?></label>
+	  <div class="a-form-row count">
+	  	<div class="a-form-field">
+	      <?php echo a_('Show %count% Event(s)', array('%count%' => $form['count']->render())) ?>
+					<div class="a-form-help collapsed"><?php echo $form['count']->renderHelp() ?></div>
+	  	</div>
+	  	<div class="a-form-error"><?php echo $form['count']->renderError() ?></div>
+	  </div>
 
-<h4 class="a-slot-form-title"><?php echo a_('Events') ?></h4>
+	  <div class="a-form-row categories">
+	    <div class="a-form-field">
+	      <label class="a-multiple-select-label" for="<?php echo $form['categories_list']->renderId() ?>"><?php echo a_('Categorized')?></label><?php echo $form['categories_list']->render() ?>
+	    		<div class="a-form-help collapsed"><?php echo $form['categories_list']->renderHelp() ?></div>
+			</div>
+	  	<div class="a-form-error"><?php echo $form['categories_list']->renderError() ?></div>
+	  </div>
 
-<div class="a-form-row count">
-	<?php echo $form['count']->renderLabel(__('Posts', array(), 'apostrophe')) ?>
-	<div class="a-form-field">
-		<?php echo $form['count']->render() ?>
-		<div class="a-form-help collapsed"><?php echo $form['count']->renderHelp() ?></div>
+	  <div class="a-form-row conjunction">
+	    <?php echo a_('And') ?>
+	  </div>
+
+	  <div class="a-form-row tags">
+	  	<div class="a-form-field">
+	  	  <label for="<?php echo $form['tags_list']->renderId() ?>"><?php echo a_('Tagged') ?></label><?php echo $form['tags_list']->render() ?>
+	      <?php $options = array('popular-tags' => $popularTags, 'tags-label' => '', 'commit-selector' => '#a-slot-form-submit-' . $id, 'typeahead-url' => url_for('taggableComplete/complete')) ?>
+	      <?php if (sfConfig::get('app_a_all_tags', true)): ?>
+	        <?php $options['all-tags'] = $allTags ?>        
+	      <?php endif ?>
+	      <?php a_js_call('pkInlineTaggableWidget(?, ?)', '#' . $form['tags_list']->renderId(), $options) ?>
+	  	</div>
+			<div class="a-form-help collapsed"><?php echo $form['tags_list']->renderHelp() ?></div>
+	  	<div class="a-form-error"><?php echo $form['tags_list']->renderError() ?></div>
+	  </div>
 	</div>
-	<div class="a-form-error"><?php echo $form['count']->renderError() ?></div>
-</div>
+	<hr />
+	<div class="a-form-row by-type title">
+	  <?php $w = $form['title_or_tag'] ?>
+	  <input type="radio" id="<?php echo $w->renderId() ?>-title" name="<?php echo $w->renderName() ?>" value="title" <?php echo ($w->getValue() === "title") ? 'checked' : '' ?> /> <label for="<?php echo $w->renderId() ?>-title"><?php echo a_('By Title') ?></label>
 
-<div class="a-form-row categories">
-	<?php echo $form['categories_list']->renderLabel(__('Category', array(), 'apostrophe')) ?>
-	<div class="a-form-field">
-		<?php echo $form['categories_list']->render() ?>
-		<div class="a-form-help collapsed"><?php echo $form['categories_list']->renderHelp() ?></div>
+	  <div class="a-form-row events">
+	    <?php echo $form['events']->render() ?>
+	  </div>
 	</div>
-	<div class="a-form-error"><?php echo $form['categories_list']->renderError() ?></div>
 </div>
-
-<div class="a-form-row tags">
-	<?php echo $form['tags_list']->renderLabel(__('Tags', array(), 'apostrophe')) ?>
-	<div class="a-form-field">
-		<?php echo $form['tags_list']->render() ?>
-		<div class="a-form-help collapsed"><?php echo $form['tags_list']->renderHelp() ?></div>
-	</div>
-	<div class="a-form-error"><?php echo $form['tags_list']->renderError() ?></div>
-</div>
-
-<script type="text/javascript" charset="utf-8" src="/sfDoctrineActAsTaggablePlugin/js/pkTagahead.js"></script>
-
 <script type="text/javascript" charset="utf-8">
 $(document).ready(function() {
     pkTagahead(<?php echo json_encode(url_for("taggableComplete/complete")) ?>);
