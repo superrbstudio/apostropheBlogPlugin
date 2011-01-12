cafce015037beddd69ca21a185474c317cb894eb
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-01-12 20:43:07 +0000

    Hit #857: next and previous buttons for blog post and event date filters needed reasonable labels. I implemented John's design using aDate::pretty (subsequently lopping off the day when browsing months rather than days). When app_a_pretty_english_dates is false I use a very conservative YYYY-MM-DD date display strategy because there is no i18n helper for "just year and month". But it's on by default in our sandbox and therefore our client projects.
    Note that if we mess with aDate::pretty we'll have to revisit this code and the way it massages the day out of it for month to month navigation
    
    Also: John's plural check needed to say != 1 rather than > 1 otherwise it didn't pluralize 0
    
    I'm going to commit this and then turn around and make it a partial for easy and inevitable overrides
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.5@3101 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_filters.php
+++ modules/aBlog/templates/_filters.php
@@ -7,9 +7,34 @@
 <div class="a-ui a-blog-browser">
 
 	<?php if ($sf_params->get('year')): ?>
+	  <?php if (sfConfig::get('app_a_pretty_english_dates')): ?>
+  	  <?php if ($sf_params->get('day')): ?>
+  	    <?php $prevLabel = aDate::pretty($params['prev']['year'] . '-' . $params['prev']['month'] . '-' . $params['prev']['day']) ?>
+  	    <?php $nextLabel = aDate::pretty($params['next']['year'] . '-' . $params['next']['month'] . '-' . $params['next']['day']) ?>
+  	  <?php elseif ($sf_params->get('month')): ?>
+  	    <?php $prevLabel = aDate::pretty($params['prev']['year'] . '-' . $params['prev']['month'] . '-01') ?>
+  	    <?php $prevLabel = preg_replace('/\s+\d+/', '', $prevLabel) ?>
+  	    <?php $nextLabel = aDate::pretty($params['next']['year'] . '-' . $params['next']['month'] . '-01') ?>
+  	    <?php $nextLabel = preg_replace('/\s+\d+/', '', $nextLabel) ?>
+  	  <?php else: ?>
+  	    <?php $prevLabel = $params['prev']['year'] ?>
+  	    <?php $nextLabel = $params['next']['year'] ?>
+  	  <?php endif ?>
+	  <?php else: ?>
+  	  <?php if ($sf_params->get('day')): ?>
+  	    <?php $prevLabel = $params['prev']['year'] . '-' . $params['prev']['month'] . '-' . $params['prev']['day'] ?>
+  	    <?php $nextLabel = $params['next']['year'] . '-' . $params['next']['month'] . '-' . $params['next']['day'] ?>
+  	  <?php elseif ($sf_params->get('month')): ?>
+  	    <?php $prevLabel = $params['prev']['year'] . '-' . $params['prev']['month'] ?>
+  	    <?php $nextLabel = $params['next']['year'] . '-' . $params['next']['month'] ?>
+  	  <?php else: ?>
+  	    <?php $prevLabel = $params['prev']['year'] ?>
+  	    <?php $nextLabel = $params['next']['year'] ?>
+  	  <?php endif ?>
+  	<?php endif ?>
 		<ul class="a-ui a-controls a-blog-browser-controls">
-	  	<li><?php echo a_button(a_('Previous'), url_for($url.'?'.http_build_query($params['prev'])), array('icon','alt','a-arrow-left', 'no-bg')) ?></li>
-	  	<li><?php echo a_button(a_('Next'), url_for($url.'?'.http_build_query($params['next'])), array('icon','alt','a-arrow-right', 'no-bg')) ?></li>
+	  	<li><?php echo a_button('&#8810; ' . $prevLabel, url_for($url.'?'.http_build_query($params['prev'])), array('icon','alt','a-arrow-left', 'no-bg')) ?></li>
+	  	<li><?php echo a_button($nextLabel . ' &#8811;', url_for($url.'?'.http_build_query($params['next'])), array('icon','alt','a-arrow-right', 'no-bg')) ?></li>
 		</ul>
 	<?php endif ?>
 
@@ -39,7 +64,7 @@
   <h3 class="a-blog-filters browser">
 	<?php if (count($filters)): ?>
 		<?php // Pluralize $type ?>
-		<?php $type = ($count > 1) ? $typePlural : $type  ?>
+		<?php $type = ($count != 1) ? $typePlural : $type  ?>
 		<?php echo a_('You are viewing %count% %type% %filters%', array('%count%' => $count, '%type%' => $type, '%filters%' => implode(' ', $filters))) ?>
 	<?php else: ?>
 		<?php if ($count): ?>
