8470f86badb22c6ef4c385a38fd667d42cbc4f7d
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-01-17 22:02:32 +0000

    doctrine:clean successfully completed. We still had rather a lot of form and filter classes for types that don't need them (fixes #430)
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.5@3181 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/form/doctrine/PluginaBlogItemForm.class.php
+++ lib/form/doctrine/PluginaBlogItemForm.class.php
@@ -215,6 +215,11 @@ abstract class PluginaBlogItemForm extends BaseaBlogItemForm
     {
       $values = $this->getValues();
     }
+
+    // Slashes break routes in most server configs. Do NOT force case of tags.
+    
+    $values['tags'] = str_replace('/', '-', isset($values['tags']) ? $values['tags'] : '');
+    
     $object = parent::updateObject($values);
     $this->updatePublication($values);
     return $object;
