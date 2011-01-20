53073a80b6097a289447b01df507c043fdbd653f
jake <jake@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-01-20 20:30:07 +0000

    disqus comment code was showing up even when disqus was disabled
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.5@3243 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_meta.php
+++ modules/aBlog/templates/_meta.php
@@ -4,7 +4,7 @@
 			<span class="a-blog-item-meta-label"><?php echo __('Posted By:', array(), 'apostrophe') ?></span>
 			<?php echo ($a_blog_post->getAuthor()->getName()) ? $a_blog_post->getAuthor()->getName() : $a_blog_post->getAuthor()  ?>
 	</li>
-	<?php  if (sfConfig::get('app_aBlog_disqus_enabled', true)): ?>
+	<?php  if (sfConfig::get('app_aBlog_disqus_enabled')): ?>
 	<li><a class="disqus-comment-count" href="<?php echo url_for('a_blog_post', $a_blog_post) ?>#disqus_thread">#</a></li>   
 	<?php endif ?>
 </ul>
