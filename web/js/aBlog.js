fcc1f6e7444f2ab83f96b18769c6804c84c9a51b
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-01-20 21:21:48 +0000

    Fixed bug where if you don't click save on the title, it returns 'Untitled' even if you have entered a title -- because the Ajax save happens before the title field gets updated.
    This bug only affects Blog 1.4
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.4@3246 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- web/js/aBlog.js
+++ web/js/aBlog.js
@@ -6,37 +6,36 @@ function aBlogUpdateForm(slug_url, event)
 	  dataType:'text',
 	  data:jQuery('#a-admin-form').serialize(),
 	  complete:function(xhr, textStatus)
-		{			
-      		if(textStatus == 'success')
-      		{
-			
-	      		var json = xhr.getResponseHeader('X-Json'); //data is a JSON object, we can handle any updates with it
-	      		var data = eval('(' + json + ')');
+		{
+			if(textStatus == 'success')
+			{
+				var json = xhr.getResponseHeader('X-Json'); //data is a JSON object, we can handle any updates with it
+				var data = eval('(' + json + ')');
 
 				if (data !== null) {
 
-		      		if ((data.modified.template !== null) && (data.modified.template !== undefined)) {
-		        		aBlogUpdateTemplate(data.template, data.feedback);
-		      		}
-
-		      		if ((data.modified.allow_comments !== null) && (data.modified.allow_comments !== undefined)) {
-		      			aBlogUpdateComments(data.aBlogPost.allow_comments); // Update Comments after ajax
+					if ((data.modified.template !== null) && (data.modified.template !== undefined))
+					{
+						aBlogUpdateTemplate(data.template, data.feedback);
 					}
 
-					aBlogPublishBtn(data.aBlogPost.status, slug_url); // Re-set Publish button after ajax
-		      		aBlogUpdateTitleAndSlug(data.aBlogPost.title, data.aBlogPost.slug); // Update Title and Slug after ajax
-					aBlogUpdateMessage('Saved!', data.aBlogPost.updated_at);
-					aBlogTitleMessage('.'+data.aBlogPost.status+'-item', data.time)	;
-					aBlogItemTitle();				
+					if ((data.modified.allow_comments !== null) && (data.modified.allow_comments !== undefined)) {
+						aBlogUpdateComments(data.aBlogPost.allow_comments); // Update Comments after ajax
+					}
 
+					aBlogUpdateTitleAndSlug(data.aBlogPost.title, data.aBlogPost.slug); // Update Title and Slug after ajax
+					aBlogPublishBtn(data.aBlogPost.status, slug_url); // Re-set Publish button after ajax
+					aBlogUpdateMessage('Saved!', data.aBlogPost.updated_at);
+					aBlogTitleMessage('.'+data.aBlogPost.status+'-item', data.time);
+					aBlogItemTitle();
 					aUI('#a-admin-form');
-					
+
 				}
 				else
 				{
-				
+
 				}
-      		}
+			}
 	 	},
 	 	url: slug_url
 	});
@@ -57,23 +56,23 @@ function aBlogPublishBtn(status, slug_url)
 	publishButton.unbind('click').click(function(){
 		$(this).blur();
 
-		if (status == 'draft') 
+		if (status == 'draft')
 		{
 			postStatus.val('published');
-			publishButton.addClass('published');			
+			publishButton.addClass('published');
 		}
 		else
 		{
 			postStatus.val('draft');
-			publishButton.removeClass('published');			
+			publishButton.removeClass('published');
 		};
-		
+
 		// If slug_url
-		if (typeof(slug_url) != 'undefined') 
+		if (typeof(slug_url) != 'undefined')
 		{
-			aBlogUpdateForm(slug_url);			
+			aBlogUpdateForm(slug_url);
 		};
-	});			
+	});
 }
 
 function aBlogGetPostStatus()
@@ -85,7 +84,7 @@ function aBlogGetPostStatus()
 
 function aBlogItemTitle(slug_url)
 {
-	// Title Interface 
+	// Title Interface
 	// =============================================
 	var titleInterface = $('#a-blog-item-title-interface');
 	var titlePlaceholder = $('#a-blog-item-title-placeholder');
@@ -93,7 +92,7 @@ function aBlogItemTitle(slug_url)
 	var tControls = titleInterface.find('ul.a-controls');
 	var originalTitle = tInput.val();
 
-	if (originalTitle == '') 
+	if (originalTitle == '')
 	{ // The blog post has no title -- Focus the input
 		tInput.focus();
 		titleInterface.addClass('active');
@@ -109,29 +108,29 @@ function aBlogItemTitle(slug_url)
 			aBlogTitleMessage('.published-item');
 		};
 	};
-	
+
 	// Title: On Change Compare
 	// Turned this off for the 1.4 Release
 	// tInput.change(function(){
 	// 	save();
 	// });
-	
+
 	tInput.blur(function()
-	{ // Check for Empty Title Field			
-		if (tInput.val() == '') 
-		{ 	
-			tInput.next().show(); 
+	{ // Check for Empty Title Field
+		if (tInput.val() == '')
+		{
+			tInput.next().show();
 		}
 	});
 
 	tInput.focus(function()
 	{	// Always hide the placeholder on focus
-		tInput.next().hide(); 
+		tInput.next().hide();
 		tInput.select();
 	});
-		
+
 	tInput.keyup(function(event){
-		if (tInput.val().trim() != originalTitle.trim())
+		if ($.trim(tInput.val()) != $.trim(originalTitle))
 		{
 			titleInterface.addClass('has-changes');
 			tControls.fadeIn();
@@ -140,14 +139,17 @@ function aBlogItemTitle(slug_url)
 	    event.preventDefault();
 			save();
 		}
+		tInput.unbind('blur.save').bind('blur.save',function(){
+			save();
+		});
 	});
 
 	titlePlaceholder.mousedown(function()
-	{	// If you click the placeholder text 
+	{	// If you click the placeholder text
 		// focus the input (Mousedown is faster than click here)
-		tInput.focus(); 
+		tInput.focus();
 	}).hide();
-	
+
 
 	// Title Interface Controls: Save | Cancel
 	// =============================================
@@ -155,20 +157,20 @@ function aBlogItemTitle(slug_url)
 	{
 		event.preventDefault();
 		$target = $(event.target);
-									
+
 		if ($target.hasClass('a-save'))
 		{
-			if (tInput.val() == '') 
-			{ 	
+			if (tInput.val() == '')
+			{
 				tInput.val(originalTitle);
-				tControls.hide();							
+				tControls.hide();
 			}
-			if ((tInput.val() != '') && (tInput.val().trim() != originalTitle.trim())) 
+			if ((tInput.val() != '') && (tInput.val().trim() != originalTitle.trim()))
 			{
 				save();
-			}										
+			}
 		}
-		
+
 		if ($target.hasClass('a-cancel'))
 		{
 			tInput.val(originalTitle);
@@ -179,21 +181,21 @@ function aBlogItemTitle(slug_url)
 	// Save Blog Title
 	function save()
 	{
-		if (typeof(slug_url) == 'string') 
+		if (typeof(slug_url) == 'string')
 		{ // If the input is not empty
 			// Pass the value to the admin form and update
 			$('#a_blog_item_title').val(tInput.val());
 			aBlogUpdateForm(slug_url);
 		};
-		tControls.hide();	
-		titleInterface.removeClass('active');		
-		tInput.effect('highlight', {}, 2000).blur();	
+		tControls.hide();
+		titleInterface.removeClass('active');
+		tInput.effect('highlight', {}, 2000);
 	}
 }
 
 function aBlogItemPermalink(slug_url)
 {
-	// Permalink Interface  
+	// Permalink Interface
 	// =============================================
 	var permalinkInterface = $('#a-blog-item-permalink-interface');
 	var pInput = permalinkInterface.find('input');
@@ -212,7 +214,10 @@ function aBlogItemPermalink(slug_url)
 			if (event.keyCode == '13') {
 		    event.preventDefault();
 				save();
-			}			
+			}
+			pInput.unbind('blur.save').bind('blur.save',function(){
+				save();
+			});
 		});
 	});
 
@@ -222,48 +227,48 @@ function aBlogItemPermalink(slug_url)
 	{
 		event.preventDefault();
 		$target = $(event.target);
-					
+
 		if ($target.hasClass('a-save'))
 		{
-			if (pInput.val() == '') 
-			{ 	
+			if (pInput.val() == '')
+			{
 				pInput.val(originalSlug);
-				pControls.hide();								
+				pControls.hide();
 			}
-			if ((pInput.val() != '') && (pInput.val().trim() != originalSlug)) 
+			if ((pInput.val() != '') && (pInput.val().trim() != originalSlug))
 			{
 				save();
-			}										
+			}
 		}
-		
+
 		if ($target.hasClass('a-cancel'))
 		{
 			pInput.val(originalSlug);
-			pControls.hide();					
+			pControls.hide();
 		}
 	});
-	
+
 	// Save Blog Title
 	function save()
 	{
-		if (pInput.val() != '') 
+		if (pInput.val() != '')
 		{ // If the input is not empty
 			// Pass the value to the admin form and update
 			$('#a_blog_item_slug').val(pInput.val());
 			aBlogUpdateForm(slug_url);
 			pControls.hide();
-			pInput.effect('highlight', {}, 2000).blur();				
-		};		
+			pInput.effect('highlight', {}, 2000);
+		};
 	}
 }
 
 function aBlogUpdateTitle(title)
 { // Update Title Function for Ajax calls when it is returned clean from Apostrophe
 	var titleInput = $('#a_blog_item_title_interface');
-		
-	if (title != null) 
+
+	if (title != null)
 	{
-		titleInput.val(title);			
+		titleInput.val(title);
 	};
 }
 
@@ -288,7 +293,7 @@ function aBlogUpdateTitleAndSlug(title, slug)
 
 function aBlogCheckboxToggle(checkbox)
 { // Toggle any checkbox you want with this one
-	checkbox.attr('checked', !checkbox.attr('checked')); 
+	checkbox.attr('checked', !checkbox.attr('checked'));
 }
 
 function aBlogUpdateComments(enabled, feedback)
@@ -299,7 +304,7 @@ function aBlogUpdateComments(enabled, feedback)
 	}
 	else
 	{
-		$('.section.comments .allow_comments_toggle').addClass('disabled').removeClass('enabled');		
+		$('.section.comments .allow_comments_toggle').addClass('disabled').removeClass('enabled');
 	}
 }
 
@@ -311,15 +316,15 @@ function aBlogUpdateTemplate(template, feedback)
 function aBlogTitleMessage(status, updated_at)
 {
 	var msgContainer = $('.a-admin-title-sentence');
-	
+
 	msgContainer.children().hide();
-	
+
 	if (typeof(updated_at) == 'string')
 	{
 		flashMsg = 	msgContainer.children('.flash-message'); flashMsg.hide();
 		var newMessage = flashMsg.clone(); var newMessageText = newMessage.text()+' <b>' + updated_at + '</b>.';
 		newMessage.html(newMessageText);
-		msgContainer.children(status).children('.flash-message').remove(); // Remove old message		
+		msgContainer.children(status).children('.flash-message').remove(); // Remove old message
 		msgContainer.children(status).show().append(newMessage); // Append new message
 		newMessage.fadeIn('slow');
 		// newMessage.fadeIn('slow').fadeTo(4000,1).fadeOut('fast', function(){
@@ -328,50 +333,50 @@ function aBlogTitleMessage(status, updated_at)
 	}
 	else
 	{
-		msgContainer.children(status).show();		
+		msgContainer.children(status).show();
 	}
 }
 
 function aBlogUpdateMessage(msg, timestamp) // I don't think this is used anymore
-{	
+{
 	if (typeof(msg) == 'undefined') {
 		msg = 'Saved!';
 	};
-	
+
 	var publishButton = $('#a-blog-publish-button');
 	var pUpdate = $('#a-blog-item-update');
 	var lastSaved = $('#post-last-saved');
-	
+
 	if (pUpdate.data('animating') != 1) {
 		pUpdate.data('animating',1).text(msg).fadeIn(100, function(){
 			publishButton.children().hide();
 			pUpdate.fadeTo(500,1, function(){
 				pUpdate.fadeOut(500, function(){
-					if (publishButton.hasClass('published')) 
+					if (publishButton.hasClass('published'))
 					{
 						publishButton.children('.unpublish').fadeIn(100);
 					}
-					else	
+					else
 					{
-						publishButton.children('.publish').fadeIn(100);					
+						publishButton.children('.publish').fadeIn(100);
 					}
 					lastSaved.find('span').text(timestamp);
 					lastSaved.fadeIn(2000, function(){
 						lastSaved.fadeTo(3000, 1, function(){
 							// lastSaved.fadeOut(); // Fade Out Message after some time
 						});
-					});					
+					});
 					pUpdate.data('animating', 0);
 				});
 			});
 		});
-	};	
+	};
 }
 
 function aBlogSendMessage(label, desc)
-{	
+{
 	// Messages are turned off for now!
-	// Send a message to the blog editor confirming a change made via Ajax	
+	// Send a message to the blog editor confirming a change made via Ajax
 	var mLabel = (label)?label.toString():""; // passed from ajaxAction
 	var mDescription = (desc)?desc.toString(): ""; // passed from ajaxAction
 	var newMessage = "<dt>"+mLabel+"</dt><dd>"+mDescription+"</dd>";
@@ -379,7 +384,7 @@ function aBlogSendMessage(label, desc)
 	messageContainer.append(newMessage).addClass('has-messages');
 	messageContainer.children('dt:last').fadeTo(5000,1).fadeOut('slow', function(){ $(this).remove(); }); // This uses ghetto fadeTo delay because jQ1.4 has built-in delay
 	messageContainer.children('dd:last').fadeTo(5000,1).fadeOut('slow', function(){	$(this).remove(); checkMessageContainer(); });  // This uses ghetto fadeTo delay because jQ1.4 has built-in delay
-	
+
 	function checkMessageContainer()
 	{
 		if (!messageContainer.children().length) {
@@ -388,19 +393,19 @@ function aBlogSendMessage(label, desc)
 	}
 }
 
-function aBlogSetDateRange(a) 
-{  
-	var b = new Date();  
-	var c = new Date(b.getFullYear(), b.getMonth(), b.getDate());  
-	if (a.id == 'a_blog_item_end_date_jquery_control') {  
-	    if ($('#a_blog_item_start_date_jquery_control').datepicker('getDate') != null) {  
-	        c = $('#a_blog_item_start_date_jquery_control').datepicker('getDate');  
+function aBlogSetDateRange(a)
+{
+	var b = new Date();
+	var c = new Date(b.getFullYear(), b.getMonth(), b.getDate());
+	if (a.id == 'a_blog_item_end_date_jquery_control') {
+	    if ($('#a_blog_item_start_date_jquery_control').datepicker('getDate') != null) {
+	        c = $('#a_blog_item_start_date_jquery_control').datepicker('getDate');
 	    }
 	  	$('#a_blog_item_end_date_jquery_control').datepicker('setDate', c);
-	}  
-	return {  
-		 minDate: c  
-	}  	
+	}
+	return {
+		 minDate: c
+	}
 }
 
 function aBlogPublishedAt(status) {
@@ -415,12 +420,12 @@ function aPopularTags(tagList, recommendedTags)
 		$(this).click(function(){
 			var theTag = $(this).text();
 			var tagSeparator = ", ";
-			var currentTags = tagList.val();			
+			var currentTags = tagList.val();
 
 				if (!$(this).hasClass('selected'))
-				{ 
+				{
 					if (currentTags == "") { tagSeparator = ""; }; // Remove separator if there are no starting tags
-					tagList.val(currentTags += tagSeparator+theTag); 
+					tagList.val(currentTags += tagSeparator+theTag);
 				}
 				else
 				{
@@ -438,6 +443,6 @@ function aPopularTags(tagList, recommendedTags)
 
 				$(this).toggleClass('selected');
 				aBlogUpdateMulti(); // call the save function after the tags are toggled
-			});	
+			});
 	});
 }
