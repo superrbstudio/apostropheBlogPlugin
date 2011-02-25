acea8de5ca3d54350b52d3996d96df2c6dabe6e7
rickybanister <rickybanister@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-02-25 20:08:35 +0000

    changed the em dash in the date range to an en dash since it's a range, en dash is the appropriate typographic character
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.5@3411 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aEvent/templates/_dateRange.php
+++ modules/aEvent/templates/_dateRange.php
@@ -20,7 +20,7 @@
   <?php echo format_date($aEvent->start_time, 't') ?>
 <?php endif ?>
 <?php if (($aEvent->start_date !== $aEvent->end_date) || ($aEvent->start_time !== $aEvent->end_time)): ?>
-  <?php echo '&mdash;' ?>
+  <?php echo '&ndash;' ?>
 <?php endif ?>
 <?php if ($aEvent->start_date !== $aEvent->end_date): ?>
   <?php echo format_date($aEvent->end_date, 'D') ?>
