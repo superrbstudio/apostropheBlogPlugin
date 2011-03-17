7fc3fd72fdecbb7b2371eaa5675658a123b1072c
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-03-17 15:26:11 +0000

    author <li> moved into author partial
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.5@3477 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_author.php
+++ modules/aBlog/templates/_author.php
@@ -2,7 +2,9 @@
 	$a_blog_post = isset($a_blog_post) ? $sf_data->getRaw('a_blog_post') : null;
 ?>
 
-<span class="a-blog-item-meta-label"><?php echo __('Posted By:', array(), 'apostrophe') ?></span>
-<?php if ($a_blog_post->getAuthor()): ?>
-  <?php echo ($a_blog_post->getAuthor()->getName()) ? $a_blog_post->getAuthor()->getName() : $a_blog_post->getAuthor()  ?>
-<?php endif ?>
+<li class="author">
+	<span class="a-blog-item-meta-label"><?php echo __('Posted By:', array(), 'apostrophe') ?></span>
+	<?php if ($a_blog_post->getAuthor()): ?>
+	  <?php echo ($a_blog_post->getAuthor()->getName()) ? $a_blog_post->getAuthor()->getName() : $a_blog_post->getAuthor()  ?>
+	<?php endif ?>
+</li>
