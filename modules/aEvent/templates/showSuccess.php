e3b7329c21e0b9f972de6ac7483446cfa1e4b01a
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-07-13 15:45:10 +0000

    Back Ported the Facebook OG Meta slot to the <head> in the bundled Apostrophe Layout Back Ported the <meta> tags to showSuccess for Blog and Events for Facebook OG Meta
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.4@3937 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aEvent/templates/showSuccess.php
+++ modules/aEvent/templates/showSuccess.php
@@ -1,3 +1,18 @@
+<?php slot('og-meta') ?>
+<?php // og-meta is meta information for Facebook that gets read when something is shared with Add This (or anything else)  ?>
+<meta property="og:title" content="<?php echo $aEvent->getTitle() ?>"/>
+<meta property="og:type" content="article"/>
+<meta property="og:url" content="<?php echo url_for('a_event', $aEvent, true) ?>"/>
+<?php $items = $aEvent->getMediaForArea('blog-body', 'image', 1) ?>
+<?php if (count($items)): ?>
+	<?php foreach ($items as $item): ?>
+<meta property="og:image" content="<?php echo $item->getImgSrcUrl(400, false, 's', 'jpg', true) ?>"/>	
+	<?php endforeach ?>
+<?php endif ?>
+<meta property="og:site_name" content="<?php echo sfContext::getInstance()->getResponse()->getTitle(); ?>"/>
+<meta property="og:description" content="<?php echo $aEvent->getTextForArea('blog-body', 25) ?>"/>
+<?php end_slot() ?>
+
 <?php slot('body_class') ?>a-blog <?php echo $sf_params->get('module'); ?> <?php echo $sf_params->get('action') ?><?php end_slot() ?>
 
 <?php slot('a-subnav') ?>
