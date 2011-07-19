451f1d1cce64536254799e743552362d1cd1bbe5
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-07-19 18:49:05 +0000

    fixed bug I introduced this morning w/ variable name
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.5@3982 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_pager.php
+++ modules/aBlog/templates/_pager.php
@@ -1,7 +1,7 @@
 <?php $pager = $sf_data->getRaw('pager') ?>
 <?php $pagerUrl = $sf_data->getRaw('pagerUrl') ?>
 <?php $max_per_page = $sf_data->getRaw('max_per_page') ?>
-<?php $pagerContainerClass = ($sf_data->getRaw('pagerContainerClass')) ? ' '.$sf_data->getRaw('pagerContainerClass') : '' ?>
+<?php $pagerContainerClass = isset($pagerContainerClass) ? ' '.$sf_data->getRaw('pagerContainerClass') : '' ?>
 
 <div class="a-ui a-media-library-controls<?php echo $pagerContainerClass ?>">
   <?php include_partial('aPager/pager', array('pager' => $pager, 'pagerUrl' => $pagerUrl)) ?>
