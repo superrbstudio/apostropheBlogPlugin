00ab6ae16b2a6bb48bcf35d35ccd15f95ee36e25
kerry <kerry@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-10-21 17:36:37 +0000

    test
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.5@4246 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- data/generator/sfDoctrineModule/aBlogAdmin/template/templates/_list_td_tabular.php
+++ data/generator/sfDoctrineModule/aBlogAdmin/template/templates/_list_td_tabular.php
@@ -1,5 +1,4 @@
 [?php use_helper('Date') ?]
-
 <?php foreach ($this->configuration->getValue('list.display') as $name => $field): ?>
 <?php echo $this->addCredentialCondition(sprintf(<<<EOF
 <td class="a-admin-%s a-column-%s">[?php echo %s ?]</td>
