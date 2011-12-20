e1e6ad63ce9fd6a61f7ca6105d72e9f26f2d181d
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-12-20 21:14:24 +0000

    more descriptive labels for the delete button in the admin form
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.5@4432 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlogAdmin/templates/_form.php
+++ modules/aBlogAdmin/templates/_form.php
@@ -134,7 +134,7 @@
 		<?php echo link_to('<span class="icon"></span>'.a_('Preview'), 'a_blog_post', array('preview' => 1) + $a_blog_post->getRoutingParams(), array('class' => 'a-btn icon a-search alt lite a-align-left', 'rel' => 'external')) ?>
 	  <?php aRouteTools::popTargetEnginePage($engine->engine) ?>
 	  <?php if($a_blog_post->userHasPrivilege('delete')): ?>
-		  <?php echo link_to('<span class="icon"></span>'.a_('Delete'), 'a_blog_admin_delete', $a_blog_post, array('class' => 'a-btn icon a-delete alt lite a-align-right', 'method' => 'delete', 'confirm' => a_('Are you sure you want to delete this post?'), )) ?>
+		  <?php echo link_to('<span class="icon"></span>'.a_('Delete Post'), 'a_blog_admin_delete', $a_blog_post, array('class' => 'a-btn icon a-delete alt lite a-align-right', 'method' => 'delete', 'confirm' => a_('Are you sure you want to delete this post?'), )) ?>
 	  <?php endif ?>
 	</div>
 	
