43699f8dd22d203e5f0a655e01dbb41b76615c48
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2012-08-28 16:06:09 +0000

    addthis support now works interchangeably with old school usernames and newfangled profile IDs (pubids)
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheBlogPlugin/branches/1.5@4810 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aBlog/templates/_addThis.php
+++ modules/aBlog/templates/_addThis.php
@@ -4,18 +4,29 @@
 	$type = $aBlogItem->getType();
 ?>
 
-<?php if ($addthis_username = sfConfig::get('app_aBlog_add_this')): ?>
+<?php $addthis_identifier = sfConfig::get('app_aBlog_add_this') ?>
+
+<?php if (strlen($addthis_identifier)): ?>
+  <?php // Recognize the new pubids and use a different keyword ?>
+  <?php if (preg_match('/^[a-z][a-z]\-[0-9a-f]+$/', $addthis_identifier)): ?>
+    <?php $keyword = 'pubid' ?>
+  <?php else: ?>
+    <?php // Old school addthis username ?>
+    <?php $keyword = 'username' ?>
+  <?php endif ?>
+
 	<!-- AddThis Button BEGIN -->
 	<div class="addthis_toolbox addthis_default_style"
 	addthis:url="<?php echo url_for( (($type == 'post') ? 'a_blog_post':'a_event_post' ), $aBlogItem, true) ?>"
 	addthis:title="<?php echo $aBlogItem['title'] ?>">
-		<a href="http://addthis.com/bookmark.php?v=250&amp;username=<?php echo $addthis_username ?>" class="addthis_button_compact">Share</a>
+		<a href="http://addthis.com/bookmark.php?v=250&amp;<?php echo $keyword ?>=<?php echo $addthis_identifier ?>" class="addthis_button_compact">Share</a>
 		<span class="addthis_separator">|</span>
 		<a class="addthis_button_facebook"></a>
 		<a class="addthis_button_myspace"></a>
 		<a class="addthis_button_google"></a>
 		<a class="addthis_button_twitter"></a>
 	</div>
-	<!-- AddThis Button END -->	
-	<?php use_javascript('http://s7.addthis.com/js/250/addthis_widget.js#username='.$addthis_username) ?>
+	<!-- AddThis Button END -->
+	<?php use_javascript('http://s7.addthis.com/js/250/addthis_widget.js#' . $keyword . '=' . $addthis_identifier) ?>
 <?php endif ?>
+
